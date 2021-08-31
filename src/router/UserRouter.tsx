c0b5a5577e7d583657e30316a9c38f205d031103
강은빈 <kub0803@naver.com>
2021-08-31 23:51:32 +0900

    [MODIFY] 유저 라우터 수정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Login } from "../components";
+import { Login, SignUp } from "../components";
 
 const UserRouter = () => {
   return (
     <BrowserRouter>
       <Route path="/login" component={Login} exact />
+      <Route path="/sign-up" component={SignUp} exact />
     </BrowserRouter>
   );
 };
