053d23802573c5b5395882f4d92c68f05caa9839
강은빈 <kub0803@naver.com>
2021-08-31 23:16:54 +0900

    [ADD] login  컴포넌트 추가

---
+++ src/components/signin/login/Login.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Github, Logo } from "../../../util/assets";
+import {
+  baseBackground,
+  ButtonWrapper,
+  center,
+  gitButton,
+  inputWrapper,
+  loginWrapper,
+} from "../../../util/css/signin/style";
+
+const Login = () => {
+  const [buttonColor, setButtonColor] = useState<boolean>(false);
+
+  return (
+    <section css={[baseBackground, center]}>
+      <form css={[loginWrapper]}>
+        <img src={Logo} alt="Portfolist 로고" />
+        <div css={[inputWrapper]}>
+          <input type="text" placeholder="이메일을 입력해주세요" />
+          <input type="password" placeholder="비밀번호를 입력해주세요" />
+        </div>
+        <ButtonWrapper btnColor={buttonColor}>
+          <button className="login-button">login</button>
+          <button css={[center, gitButton]}>
+            <img src={Github} alt="깃허브 로고"></img>
+            <span>Github 로그인</span>
+          </button>
+        </ButtonWrapper>
+        <Link
+          to="/sign-in"
+          css={css`
+            color: #8b8b8b;
+          `}
+        >
+          아직 계정이 없으신가요?
+        </Link>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
---
+++ src/util/css/signin/style.ts
@@ -0,0 +1,74 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const center = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const baseBackground = css`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const loginWrapper = css`
+  width: 300px;
+  height: 440px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const inputWrapper = css`
+  margin-top: 60px;
+
+  & input {
+    margin-bottom: 10px;
+    font-weight: 500;
+    width: 98%;
+    height: 35px;
+    padding: 1%;
+    outline: none;
+    border: none;
+    border-bottom: 2px solid #c4c4c4;
+  }
+`;
+
+interface buttonType {
+  btnColor: boolean;
+}
+
+export const ButtonWrapper = styled.div`
+  margin-top: 50px;
+
+  & button {
+    margin-bottom: 30px;
+    color: white;
+    font-weight: 500;
+    font-size: 18px;
+    width: 300px;
+    height: 50px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.3s;
+    filter: drop-shadow(0px 2px 7px rgba(119, 119, 119, 0.25));
+
+    & span {
+      margin-left: 10px;
+    }
+  }
+
+  .login-button {
+    background: ${({ btnColor }: buttonType) =>
+      btnColor ? "#FF7659" : "#DADADA"};
+  }
+`;
+
+export const gitButton = css`
+  background-color: #4d4d4d;
+
+  :hover {
+    background-color: black;
+  }
+`;
