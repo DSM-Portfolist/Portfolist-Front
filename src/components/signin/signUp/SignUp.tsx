2bbbc0660259a2568890f7d1f0d63d60b9864ef1
강은빈 <kub0803@naver.com>
2021-09-01 16:37:50 +0900

    [MODIFY] 회원가입 코드 수정

--- src/components/signin/signUp/SignUp.tsx
+++ src/components/signin/signUp/SignUp.tsx
@@ -1,50 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
-import { useState } from "react";
-import { Logo } from "../../../util/assets";
-import {
-  baseBackground,
-  ButtonWrapper,
-  center,
-  inputItem,
-  inputWrapper,
-  loginWrapper,
-  NextButton,
-  row,
-  signUpWrapper,
-} from "../../../util/css/signin/style";
-import ProgressBar from "./ProgressBar";
+import { baseBackground, center } from "../../../util/css/signin/style";
+import FirstProgress from "./FirstProgress";
 
 const SignUp = () => {
-  const [btnColor, setBtnColor] = useState<boolean>(false);
-
   return (
     <section css={[baseBackground, center]}>
-      <form css={[loginWrapper, signUpWrapper]}>
-        <img src={Logo} alt="Portfolist 로고" />
-        <ProgressBar />
-        <div css={[inputWrapper]}>
-          <div css={[row, inputItem]}>
-            <span>이름</span>
-            <input type="text" placeholder="사용하실 이름을 입력해주세요" />
-          </div>
-          <div css={[row, inputItem]}>
-            <span>이메일</span>
-            <input type="text" placeholder="사용하실 이메일을 입력해주세요" />
-          </div>
-          <div css={[row, inputItem]}>
-            <span>비밀번호</span>
-            <input type="text" placeholder="사용하실 이름을 입력해주세요" />
-          </div>
-          <div css={[row, inputItem]}>
-            <span>비밀번호 확인</span>
-            <input type="text" placeholder="사용하실 이메일을 입력해주세요" />
-          </div>
-        </div>
-        <NextButton>
-          <button>다음</button>
-        </NextButton>
-      </form>
+      <FirstProgress />
     </section>
   );
 };
