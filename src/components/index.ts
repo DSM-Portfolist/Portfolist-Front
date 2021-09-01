16c71a683c6dcdd1752ce828b6c69566879482e6
강은빈 <kub0803@naver.com>
2021-09-01 16:34:36 +0900

    [ADD] 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Login } from "./signin/login/Login";
 export { default as SignUp } from "./signin/signUp/SignUp";
+export { default as FirstProgress } from "./signin/signUp/FirstProgress";
