fb7274441f54ee08a21b085972e8ddc49d2707b6
JaeHyun <soehd0889@naver.com>
2021-09-01 19:16:11 +0900

    [ADD] 라우팅 설정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Login, SignUp } from "../components";
+import MyPage from "../components/mypage/mypage/MyPage";
+import UserPage from "../components/mypage/userPage/UserPage";
 
 const UserRouter = () => {
   return (
     <BrowserRouter>
       <Route path="/login" component={Login} exact />
       <Route path="/sign-up" component={SignUp} exact />
-
-      <Route path="/my-mypage" component={MyMypage} exact />
-      <Route path="/your-mypage" component={YourMypage} exact />
+      <Route path="/mypage" component={MyPage} exact />
+      <Route path="/user-page" component={UserPage} exact />
     </BrowserRouter>
   );
 };
