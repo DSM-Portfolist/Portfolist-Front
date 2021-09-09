0752fc8c042a6dc5edf783c7ccbf62cd45d4fbbd
강은빈 <kub0803@naver.com>
2021-09-09 23:29:29 +0900

    [ADD] 라우터  컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from "./signin/login/Login";
 export { default as SignUp } from "./signin/signUp/SignUp";
 export { default as FirstProgress } from "./signin/signUp/FirstProgress";
+export { default as SecProgress } from "./signin/signUp/SecProgress";
--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Login, SignUp } from "../components";
+import { Login, SecProgress, SignUp } from "../components";
 
 const UserRouter = () => {
   return (
     <BrowserRouter>
       <Route path="/login" component={Login} exact />
       <Route path="/sign-up" component={SignUp} exact />
+      <Route path="/sign-up/sec" component={SecProgress} exact />
     </BrowserRouter>
   );
 };
