d78c8e174d1895b2d494b04098b0de36d2cc7096
강은빈 <kub0803@naver.com>
2021-09-14 23:32:05 +0900

    [ADD] header UI

---
+++ src/components/main/Main/Main.tsx
@@ -0,0 +1,13 @@
+import react from "react";
+import Header from "../header/Header";
+import * as S from "./style";
+
+const Main = () => {
+  return (
+    <S.MainWrapper>
+      <Header />
+    </S.MainWrapper>
+  );
+};
+
+export default Main;
