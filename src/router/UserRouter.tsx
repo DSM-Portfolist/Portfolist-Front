3e341f6186d8cbc08388123dda5a70b8a558e160
강은빈 <kub0803@naver.com>
2021-09-14 23:32:17 +0900

    [ADD] main 추가

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Login, SecProgress, SignUp } from "../components";
+import { Login, Main, SecProgress, SignUp } from "../components";
 
 const UserRouter = () => {
   return (
     <BrowserRouter>
+      <Route path="/" component={Main} exact />
       <Route path="/login" component={Login} exact />
       <Route path="/sign-up" component={SignUp} exact />
       <Route path="/sign-up/sec" component={SecProgress} exact />
