587e9b8ffd99bf6fbe89ec3ddda3178ef8e76f39
JaeHyun <soehd0889@naver.com>
2021-09-14 12:15:31 +0900

    [MODIFY] 조건부 랜더링을 위한 경로설정, 스타일

--- src/components/mypage/ProfileHeader/ProfileHeader.tsx
+++ src/components/mypage/ProfileHeader/ProfileHeader.tsx
@@ -1,15 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
-import { column } from "../../../../util/css/mypage/UserPage/style";
-import { row } from "../../../../util/css/signin/style";
+import { column } from "../../../util/css/mypage/UserPage/style";
+import { row } from "../../../util/css/signin/style";
 import {
   profileBottom,
   profileHeader,
   profileImage,
   profileWrapper,
-} from "../../../../util/css/mypage/UserPage/ProfileHeader/style";
+} from "../../../util/css/mypage/ProfileHeader/style";
+import { center } from "../../../util/css/mypage/mypage/style";
 
-const ProfileHeader = () => {
+const ProfileHeader = (props: any) => {
+  const { isMypage } = props;
   const baseProfileImage =
     "https://yt3.ggpht.com/ytc/AKedOLSYskiGDIyLCoFETAvHx4tHvHzlBKkpj3pJ36lpwQ=s900-c-k-c0x00ffffff-no-rj";
 
@@ -24,6 +26,7 @@ const ProfileHeader = () => {
           <p>프론트엔드</p>
           <h3>직업</h3>
           <p>학생</p>
+          {isMypage ? <button css={[center]}>프로필 수정</button> : ""}
         </div>
       </div>
     </header>
