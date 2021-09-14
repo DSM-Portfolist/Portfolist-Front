30bf8cb3db45129367692e2e0ef72f25e559a0ac
강은빈 <kub0803@naver.com>
2021-09-14 23:32:35 +0900

    [ADD] header ui

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import { Logo, Profile } from "../../../util/assets";
+import { Logo, NoNotification, Profile } from "../../../util/assets";
 import * as S from "./style";
 
 const Header = () => {
   return (
     <S.HeaderWrapper>
-      <img src={Logo} alt="포트폴리스트 로고" />
-      <ul>
-        <li>portfolio 구경</li>
-        <li>portfolio 제작</li>
-      </ul>
-      <ul>
-          <li><img src="" alt="" /></li>
-        <li>강은빈님</li>
-        <li>
-          <img src={Profile} alt="프로필 사진" />
-        </li>
-
-      </ul>
+      <S.Container>
+        <img src={Logo} alt="포트폴리스트 로고" />
+        <ul>
+          <li>portfolio 구경</li>
+          <li>portfolio 제작</li>
+        </ul>
+        <ul>
+          <li>
+            <img src={NoNotification} alt="알림아이콘" />
+          </li>
+          <li>강은빈님</li>
+          <li>
+            <img src={Profile} alt="프로필 사진" />
+          </li>
+        </ul>
+      </S.Container>
     </S.HeaderWrapper>
   );
 };
