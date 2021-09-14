c450aeb3af0629a85e76b5d6d79e37ea85a05d59
JaeHyun <soehd0889@naver.com>
2021-09-14 12:15:50 +0900

    [ADD] 마이페이지 기본 style

--- src/util/css/mypage/mypage/style.ts
+++ src/util/css/mypage/mypage/style.ts
@@ -1,1 +1,23 @@
 import { css } from "@emotion/react";
+
+export const baseBackground = css`
+  height: 200vh;
+  overflow-x: hidden;
+  align-items: center;
+`;
+
+export const center = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const column = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const row = css`
+  display: flex;
+  flex-direction: row;
+`;
