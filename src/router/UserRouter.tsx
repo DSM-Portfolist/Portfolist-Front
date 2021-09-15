01acaa40b60214b3467356a1a605c50151263b66
JaeHyun <soehd0889@naver.com>
2021-09-15 19:03:18 +0900

    [ADD] 나의 페이지 경로 설정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Login, Main, SecProgress, SignUp } from "../components";
+import { Login, SignUp } from "../components";
 import MyPage from "../components/mypage/mypage/Mypage";
 import UserPage from "../components/mypage/userPage/UserPage";
+import MypageModify from "../components/mypage/mypage/mypageModify/MypageModify";
 
 const UserRouter = () => {
   return (
     <BrowserRouter>
-      <Route path="/" component={Main} exact />
       <Route path="/login" component={Login} exact />
       <Route path="/sign-up" component={SignUp} exact />
-      <Route path="/sign-up/sec" component={SecProgress} exact />
       <Route path="/my-page" component={MyPage} exact />
       <Route path="/user-page" component={UserPage} exact />
+      <Route path="/my-page-modify" component={MypageModify} exact />
     </BrowserRouter>
   );
 };
