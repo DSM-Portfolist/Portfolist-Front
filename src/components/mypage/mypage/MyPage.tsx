f8ddc23640058fed0b4da46a8fd2da656b0102a8
JaeHyun <soehd0889@naver.com>
2021-09-15 14:40:50 +0900

    [ADD] 마이페이지 컴포넌트 구현

--- src/components/mypage/mypage/MyPage.tsx
+++ src/components/mypage/mypage/MyPage.tsx
@@ -1,9 +1,50 @@
-
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import React, { useState } from "react";
+import {
+  baseBackground,
+  center,
+  NavWrapper,
+  column,
+  row,
+} from "../../../util/css/mypage/mypage/style";
+import {
+  myPageSection,
+  sectionTitleWrapper,
+} from "../../../util/css/mypage/UserPage/style";
+import ProfileHeader from "../ProfileHeader/ProfileHeader";
+import PortfolioList from "../userPage/PortfolioList/PortfolioList";
 
 const MyPage = () => {
-  return <h1>안녕</h1>
-}
+  const [isClickMyPortfolio, setIsClickMyPortfolio] = useState<boolean>(false);
+  const [isClickMyTouching, setIsClickMyTouching] = useState<boolean>(false);
+  console.log(isClickMyPortfolio, isClickMyTouching);
+  const onClickEvent = (e: any) => {
+    if (e.target.innerHTML === "나의 포트폴리오") {
+      !isClickMyPortfolio && setIsClickMyPortfolio(true);
+      setIsClickMyTouching(false);
+    } else if (e.target.innerHTML === "나의 터칭") {
+      !isClickMyTouching && setIsClickMyTouching(true);
+      setIsClickMyPortfolio(false);
+    }
+    console.log(e);
+  };
+  return (
+    <div css={[baseBackground, column]}>
+      <header></header>
+      <section css={[myPageSection]}>
+        <ProfileHeader isMypage={true} />
+        <article>
+          <div css={[center, sectionTitleWrapper]}>
+            <NavWrapper isClickMyPortfolio={isClickMyPortfolio} isClickMyTouching={isClickMyTouching}>
+              <h1 onClick={onClickEvent}>나의 포트폴리오</h1>
+              <h1 onClick={onClickEvent}>나의 터칭</h1>
+            </NavWrapper>
+          </div>
+          <PortfolioList />
+        </article>
+      </section>
+    </div>
+  );
+};
 
 export default MyPage;
