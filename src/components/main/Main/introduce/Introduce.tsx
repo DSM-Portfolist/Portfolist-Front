0efbd9f0c20fdbbf6e0d6bbd042164793e24326d
강은빈 <kub0803@naver.com>
2021-09-23 23:09:34 +0900

    [MODIFY] 메인페이지 소개 스타일 수정

--- src/components/main/Main/introduce/Introduce.tsx
+++ src/components/main/Main/introduce/Introduce.tsx
@@ -1,53 +1,54 @@
-import react from "react";
 import { MainContentItem } from "../../../../util/assets";
 import * as S from "./style";
 
 const Introduce = () => {
   return (
-    <S.IntroduceWrapper>
-      <div className="content">
-        <span>이제 당신의 포트폴리오를</span>
-        <div className="content-item">
-          <S.Portfolist className="portfolist">portfolist </S.Portfolist>
-          <span>로 제작해보세요.</span>
+    <div className="border-bottom">
+      <S.IntroduceWrapper>
+        <div className="content">
+          <span>이제 당신의 포트폴리오를</span>
+          <div className="content-item">
+            <S.Portfolist className="portfolist">portfolist </S.Portfolist>
+            <span>로 제작해보세요.</span>
+          </div>
         </div>
-      </div>
-      <div className="content">
-        <div className="content-item">
-          <span>다양한 개발자들과 포트폴리오를&nbsp;</span>
-          <S.PortItem>
-            <span>공유</span>
-            <img
-              className="content-icon"
-              src={MainContentItem}
-              alt="강조 아이콘"
-            />
-          </S.PortItem>
-          <span>하고</span>
+        <div className="content">
+          <div className="content-item">
+            <span>다양한 개발자들과 포트폴리오를&nbsp;</span>
+            <S.PortItem>
+              <span>공유</span>
+              <img
+                className="content-icon"
+                src={MainContentItem}
+                alt="강조 아이콘"
+              />
+            </S.PortItem>
+            <span>하고</span>
+          </div>
+          <div className="content-item">
+            <span>마음껏&nbsp;</span>
+            <S.PortItem>
+              <span>소통&nbsp;</span>
+              <img
+                className="content-icon"
+                src={MainContentItem}
+                alt="강조 아이콘"
+              />
+            </S.PortItem>
+            <span>해보세요.</span>
+          </div>
         </div>
-        <div className="content-item">
-          <span>마음껏&nbsp;</span>
-          <S.PortItem>
-            <span>소통&nbsp;</span>
-            <img
-              className="content-icon"
-              src={MainContentItem}
-              alt="강조 아이콘"
-            />
-          </S.PortItem>
-          <span>해보세요</span>
+        <div className="content">
+          <span>많은 포트폴리오를 구경하고 자신만의</span>
+          <div className="content-item">
+            <S.Porfolio className="portfolio">포트폴리오</S.Porfolio>
+            <span>를 만들어 보아요.</span>
+          </div>
         </div>
-      </div>
-      <div className="content">
-        <span>많은 포트폴리오를 구경하고 자신만의</span>
-        <div className="content-item">
-          <S.Porfolio className="portfolio">포트폴리오</S.Porfolio>
-          <span>를 만들어 보아요</span>
-        </div>
-      </div>
 
-      <button>시작하기</button>
-    </S.IntroduceWrapper>
+        <button>시작하기</button>
+      </S.IntroduceWrapper>
+    </div>
   );
 };
 
