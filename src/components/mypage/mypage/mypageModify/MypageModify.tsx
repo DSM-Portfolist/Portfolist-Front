d7156be353a2028f5b89363185e8927db2387f16
JaeHyun <soehd0889@naver.com>
2021-09-23 22:36:09 +0900

    [ADD] 이미지 업로드 컴포넌트화

--- src/components/mypage/mypage/mypageModify/MypageModify.tsx
+++ src/components/mypage/mypage/mypageModify/MypageModify.tsx
@@ -1,56 +1,22 @@
 /** @jsxImportSource @emotion/react */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   MainSection,
   MyProfileWrapper,
-  ProfileImageWrapper,
 } from "../../../../util/css/mypage/mypage/mypageModify/style";
-import { profileImage } from "../../../../util/css/mypage/ProfileHeader/style";
 import {
   baseBackground,
   column,
 } from "../../../../util/css/mypage/mypage/style";
+import ImageUploadWrapper from "./ImageUploadWrapper";
 
 const MypageModify = () => {
-  const [imageFile, setImageFile] = useState<any>("");
-  const [previewURL, setPreviewURL] = useState<any>("");
-
-  console.log(previewURL);
-
-  let previewImage = <img css={profileImage} alt="" />;
-
-  imageFile !== "" &&
-    (previewImage = (
-      <img css={profileImage} alt="" src={previewURL} />
-    ));
-
-  const handleFileOnChange = (e: any) => { //이미지 파일 브리뷰
-    e.preventDefault();
-    let reader = new FileReader();
-    let file = e.target.files[0];
-    reader.onloadend = () => {
-      setImageFile(file);
-      setPreviewURL(reader.result);
-    };
-    reader.readAsDataURL(file);
-  };
-
   return (
     <div css={[baseBackground, column]}>
       <header></header>
       <MainSection>
         <MyProfileWrapper>
-          <ProfileImageWrapper>
-            {previewImage}
-            <input
-              type="file"
-              accept="image/jpg,impge/png,image/jpeg,image/gif"
-              onChange={handleFileOnChange}
-              style={{ display: "none" }}
-              id="input-file"
-            />
-            <label htmlFor="input-file">이미지 업로드</label>
-          </ProfileImageWrapper>
+          <ImageUploadWrapper />
         </MyProfileWrapper>
       </MainSection>
     </div>
--- src/util/css/mypage/mypage/mypageModify/style.ts
+++ src/util/css/mypage/mypage/mypageModify/style.ts
@@ -20,10 +20,7 @@ export const ProfileImageWrapper = styled.div`
   padding-right: 22px;
   border-right: solid #e7e7e7 2px;
   & img {
-    margin-top: 15px;
-  }
-  & img::after {
-    content: "이미지";
+    margin-top: 12px;
   }
   & label {
     display: flex;
@@ -31,16 +28,33 @@ export const ProfileImageWrapper = styled.div`
     align-items: center;
     margin-top: 25px;
     width: 100%;
-    cursor: pointer;
     height: 40px;
+    cursor: pointer;
     background: #ff7d64;
     border-radius: 4px;
     box-shadow: 0px 4px 8px rgba(153, 153, 153, 0.25);
     font-family: "Noto Sans KR";
     font-style: 500;
-    font-weight: bold;
     font-size: 18px;
     line-height: 26px;
     color: #ffffff;
+  }
+  & div {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 26px;
+    box-shadow: 0px 4px 8px rgba(153, 153, 153, 0.25);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid 2px #ff7d64;
+    border-radius: 4px;
+    color: #ff7d64;
+    margin-top: 25px;
+    cursor: pointer;
+    width: 100%;
+    height: 38px;
   }
 `;
