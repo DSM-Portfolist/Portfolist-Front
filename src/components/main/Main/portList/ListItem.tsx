e102bc8e06bdc2a2dacb268b7c0c97301e77032f
강은빈 <kub0803@naver.com>
2021-09-23 22:39:39 +0900

    [ADD] 리스트 아이템 추가

--- src/components/main/Main/portList/ListItem.tsx
+++ src/components/main/Main/portList/ListItem.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Flower } from "../../../../util/assets";
 import * as S from "./style";
 
-const ListItem = () => {
+interface ListProps {
+  key?: number;
+  title: string;
+  content: string;
+  id?: number;
+}
+
+const ListItem = ({ key, title, content }: ListProps) => {
+  function TestSlice(txt: string) {
+    let len = 13;
+    if (txt.length > len) {
+      txt = txt.substr(0, len) + " ...";
+    }
+    return txt;
+  }
+
   return (
-    <S.ItemWrapper>
-      <img src={Flower} alt="포트폴리오 이미지 " />
+    <S.ItemWrapper key={key}>
+      <div className="folio-img">
+        <img src={Flower} alt="포트폴리오 이미지 " />
+      </div>
+      <div className="more-explore">
+        <span>{title}</span>
+        <span>{TestSlice(content)}</span>
+      </div>
     </S.ItemWrapper>
   );
 };
