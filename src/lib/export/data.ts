a85751f66eed72a5caea75505cd878769d7465d6
JaeHyun <soehd0889@naver.com>
2021-09-23 23:13:24 +0900

    [ADD] 더미데이터 추가

---
+++ src/lib/export/data.ts
@@ -0,0 +1,22 @@
+export const portfolioArr = [
+  {
+    job: "학생",
+    field: "백 엔드",
+    title: "💖 침착맨",
+    content: "트위치 방송 스트리머 침착맨 입니다!",
+    username: "침착맨",
+    commment_num: "7",
+    touching_num: "6",
+    date: "2021-08-25",
+  },
+  {
+    job: "학생",
+    field: "백 엔드",
+    title: "💖 침착맨",
+    content: "트위치 방송 스트리머 침착맨 입니다!",
+    username: "침착맨",
+    commment_num: "7",
+    touching_num: "6",
+    date: "2021-08-25",
+  },
+];
