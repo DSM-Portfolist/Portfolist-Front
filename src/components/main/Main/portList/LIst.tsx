2de53692c0d67618edf10782808af398a94e40ea
강은빈 <kub0803@naver.com>
2021-09-23 12:10:45 +0900

    [ADD] 포트폴리오 리스트 컴포넌트 추가

--- src/components/main/Main/portList/LIst.tsx
+++ src/components/main/Main/portList/LIst.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Arrow } from "../../../../util/assets";
+import ListItem from "./ListItem";
 import * as S from "./style";
+import { list } from "./dummy.json";
 
 const List = () => {
-  return <S.ListWrapper></S.ListWrapper>;
+  return (
+    <div className="border-bottom">
+      <S.ListWrapper>
+        <S.GoWrapper>
+          <p>portfolio</p>
+          <button>
+            <span>Go</span>
+            <img src={Arrow} alt="화살표" />
+          </button>
+        </S.GoWrapper>
+        <S.ListItemWrapper>
+          {list.map((list, index) => (
+            <ListItem
+              key={index}
+              title={list.title}
+              content={list.content}
+              id={list.id}
+            />
+          ))}
+        </S.ListItemWrapper>
+      </S.ListWrapper>
+    </div>
+  );
 };
 
 export default List;
