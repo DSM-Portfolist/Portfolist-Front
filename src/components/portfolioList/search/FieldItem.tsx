22d414d8b88782ca65b6babfd1923a2559c1adce
강은빈 <kub0803@naver.com>
2021-09-25 23:46:39 +0900

    [MODIFY] props 수정

--- src/components/portfolioList/search/FieldItem.tsx
+++ src/components/portfolioList/search/FieldItem.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { CloseIcon } from "../../../util/assets";
 import * as S from "./style";
 
-const FieldItem = () => {
+interface Props {
+  field: string;
+}
+
+const FieldItem = ({ field }: Props) => {
   return (
     <S.FieldItemWrapper>
-      <span>프론트엔드</span>
+      <span>{field}</span>
       <img src={CloseIcon} alt="닫기 아이콘" />
     </S.FieldItemWrapper>
   );
