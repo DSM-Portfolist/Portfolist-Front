f18b7a503882beb581176411c32a5b970ef718c6
강은빈 <kub0803@naver.com>
2021-09-25 21:47:38 +0900

    [PEAT] 리스트 값 넘기기 기능

--- src/components/portfolioList/list/MainList.tsx
+++ src/components/portfolioList/list/MainList.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ListItem from "./ListItem";
 import * as S from "./style";
+import { list } from "./dummy.json";
 
 const MainList = () => {
   return (
     <>
       <S.MainListWrapper>
         <S.ListWrapper>
-          <ListItem />
+          {list.map((list: any, index) => (
+            <ListItem
+              key={index}
+              user={list.user}
+              url={list.url}
+              field={list.field}
+              title={list.title}
+              introduce={list.introduce}
+              touching={list.touching}
+              touched={list.touched}
+            />
+          ))}
         </S.ListWrapper>
       </S.MainListWrapper>
     </>
