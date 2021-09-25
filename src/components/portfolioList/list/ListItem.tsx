7e900016656abdda85c04d643e76a73aa522ec51
강은빈 <kub0803@naver.com>
2021-09-25 21:49:29 +0900

    [PEAT] 터칭 / 포트폴리오 이동 / 스타일

--- src/components/portfolioList/list/ListItem.tsx
+++ src/components/portfolioList/list/ListItem.tsx
@@ -1,26 +1,75 @@
-import React from "react";
-import { Flower, Profile } from "../../../util/assets";
+import React, { useState } from "react";
+import {
+  BeforeTouching,
+  Flower,
+  Profile,
+  Touching,
+} from "../../../util/assets";
 import * as S from "./style";
+import { PortListType } from "../../../util/interface/main/portListType";
 
-const Tag = ({ content }: any) => {
-  return <S.Tag>{content}</S.Tag>;
+const Tag = ({ field }: any) => {
+  return <S.Tag>{field}</S.Tag>;
 };
 
-const ListItem = () => {
+const ListItem = ({
+  url,
+  field,
+  title,
+  user,
+  introduce,
+  touching,
+  touched,
+}: PortListType) => {
+  const [touchingBoolean, setTouchingBoolean] = useState<boolean>(touched);
+  const [count, setCount] = useState<number>(touching);
+
+  function CountChangeHandler(count: number) {
+    touchingBoolean ? setCount(count - 1) : setCount(count + 1);
+  }
+
+  function TextSliceHandler(txt: string, len: number) {
+    if (txt.length > len) {
+      txt = txt.substr(0, len) + " ...";
+    }
+
+    return txt;
+  }
+
   return (
     <S.ListItemWrapper>
-      <img src={Flower} alt="포트폴리오 배너" />
-      <S.Content>
-        <div className="tag">
-          <Tag content={"학생"}></Tag>
+      <div className="portfoilo-img">
+        <img src={Flower} alt="포트폴리오 배너" />
+      </div>
+      <S.Content touchingBoolean={touchingBoolean}>
+        <div className="tag-wrapper">
+          <div className="tag">
+            {field.map((field, index) => (
+              <Tag key={index} field={field} />
+            ))}
+          </div>
+          <div className="touching">
+            <img
+              src={touchingBoolean ? Touching : BeforeTouching}
+              alt="터칭 아이콘"
+              onClick={() => {
+                console.log(touchingBoolean);
+                setTouchingBoolean(!touchingBoolean);
+                CountChangeHandler(count);
+              }}
+            />
+            <span>{count === 0 ? "" : count}</span>
+          </div>
         </div>
         <div className="title">
-          <span>강은빈</span>
-          <span>금처럼 반짝반짝 빛나고 있는 강은빈입니다.</span>
+          <span>{title}</span>
+          <span>{TextSliceHandler(introduce, 32)}</span>
         </div>
         <div className="user-profile">
           <img src={Profile} alt="사용자의 프로필 사진" />
-          <span>강은빈님이 포트폴리오</span>
+          <span>
+            <strong>{user.name}</strong>님이 포트폴리오
+          </span>
         </div>
       </S.Content>
     </S.ListItemWrapper>
--- src/components/portfolioList/list/style.ts
+++ src/components/portfolioList/list/style.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
-import { portfolioBackColor } from "../../../util/css/color/color";
+import { mainColor, portfolioBackColor } from "../../../util/css/color/color";
+import { TouchingProp } from "../../../util/interface/main/mainType";
 
 export const MainListWrapper = styled.section`
   background-color: ${portfolioBackColor};
@@ -10,40 +11,132 @@ export const MainListWrapper = styled.section`
 export const ListWrapper = styled.div`
   margin: 0 auto;
   width: 1400px;
-  height: 1000px;
+  min-height: 1000px;
   background-color: white;
-
-  img {
-    width: 100%;
-    height: 190px;
-    object-fit: cover;
-  }
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
 
 export const ListItemWrapper = styled.div`
-  width: 240px;
-  height: 360px;
+  margin: 25px;
+  width: 220px;
+  height: 390px;
   box-shadow: 0px 4px 4px rgba(214, 214, 214, 0.25);
+  overflow: hidden;
+  cursor: pointer;
+
+  .portfoilo-img {
+    width: 100%;
+    height: 190px;
+    transition: opacity 0.3s ease 0s, transform 0.3s ease 0s;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      overflow: hidden;
+    }
+  }
+
+  :hover .portfoilo-img {
+    transform: scale(1.1);
+  }
 `;
 
 export const Content = styled.div`
-  width: 100%;
+  width: 90%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 0 auto;
 
   .user-profile {
+    padding-bottom: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
     img {
+      margin: 0 5px;
       width: 30px;
       height: 30px;
       border-radius: 50px;
       object-fit: cover;
     }
+
+    span {
+      margin-top: 3px;
+      font-size: 14px;
+    }
+  }
+
+  .tag-wrapper {
+    margin-top: 10px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    .tag {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .touching {
+      width: 35px;
+      height: 45px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      img {
+        cursor: pointer;
+        width: 25px;
+        height: 30px;
+        object-fit: cover;
+      }
+
+      span {
+        color: ${({ touchingBoolean }: TouchingProp) =>
+          touchingBoolean ? `${mainColor}` : "#C5C5C5"};
+        height: 22px;
+      }
+    }
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+
+    span:nth-child(1) {
+      font-weight: 500;
+      font-size: 16px;
+    }
+
+    span:nth-child(2) {
+      margin: 8px 0 10px 0;
+      font-weight: 400;
+      color: #8e8e8e;
+      font-size: 14px;
+    }
   }
 `;
 
 export const Tag = styled.div`
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  padding: 3px 8px;
+  margin-right: 4px;
+  align-items: center;
   background: #ff7659;
   border-radius: 20px;
   color: white;
-  padding: 3px;
-  font-size: 13px;
+  font-size: 11px;
   font-weight: 400;
 `;
