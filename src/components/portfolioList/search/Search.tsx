0deeb30c90d154da791dbbdf1197ae5643cf49f6
강은빈 <kub0803@naver.com>
2021-09-26 09:30:12 +0900

    [PEAT] 필터 5개 이상 검색 금지 기능

--- src/components/portfolioList/search/Search.tsx
+++ src/components/portfolioList/search/Search.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastError } from "../../../hook/toastHook";
 import { NoSelectedArrow } from "../../../util/assets";
 import FieldItem from "./FieldItem";
 import FieldSelectItem from "./FieldSelectItem";
@@ -8,10 +11,18 @@ const Search = () => {
   const [text, setText] = useState<string>("원하는 분야를 선택해주세요.");
   const [arrowSelect, setArrowSelect] = useState<boolean>(false);
   const [bottomLine, setBottomList] = useState<boolean>(false);
-  const [useField, setUseField] = useState<any>(["프론트엔드"]);
+  const [useField, setUseField] = useState<any>([]);
+
+  useEffect(() => {
+    if (useField.length >= 5) {
+      ToastError("필터는 최대 5개까지 가능합니다.");
+      console.log(false);
+    }
+  }, [useField]);
 
   return (
     <S.SearchWrapper>
+      <ToastContainer />
       <S.FieldSelectWrapper arrowSelect={arrowSelect}>
         <span>분야</span>
         <div className="field-select">
@@ -32,6 +43,7 @@ const Search = () => {
           </div>
           <FieldSelectItem
             arrowSelect={arrowSelect}
+            setArrowSelect={setArrowSelect}
             setText={setText}
             setUseField={setUseField}
             useField={useField}
