3eec725f32ef210d54117733744a921bb9dc8e1b
강은빈 <kub0803@naver.com>
2021-09-26 23:10:59 +0900

    [ADD] 댓글 interface 추가

---
+++ src/util/interface/main/commentType.ts
@@ -0,0 +1,26 @@
+export interface CommentType {
+  user: CommentUser;
+  comment_id: number;
+  comment_content: string;
+  c_date: string;
+  re_comment: ReCommentType[];
+}
+
+export interface ReCommentType {
+  user: ReCommentUser;
+  re_comment_id: number;
+  re_comment_content: string;
+  rc_date: string;
+}
+
+export interface CommentUser {
+  c_user_id: number;
+  c_name: string;
+  c_profile: string;
+}
+
+export interface ReCommentUser {
+  rc_user_id: number;
+  rc_name: string;
+  rc_profile: string;
+}
