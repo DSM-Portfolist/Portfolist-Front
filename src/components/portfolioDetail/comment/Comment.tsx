b2a4a5f8d845dec23ab5187f81407ac2e046435c
강은빈 <kub0803@naver.com>
2021-09-26 21:53:31 +0900

    [ADD] 댓글 리스트 컴포넌트 추가

--- src/components/portfolioDetail/comment/Comment.tsx
+++ src/components/portfolioDetail/comment/Comment.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import CommentItem from "./CommentItem";
 import * as S from "./style";
+import { comment } from "../dummy.json";
 
 const Comment = () => {
-  return <></>;
+  return (
+    <>
+      <S.CommentWrapper>
+        <S.InputWrapper>
+          <textarea placeholder="댓글을 입력해주세요" />
+          <button>등록</button>
+        </S.InputWrapper>
+        <S.CommentList>
+          <div className="comment-info">
+            <span>댓글 2개</span>
+          </div>
+        </S.CommentList>
+        {/*  {comment.map((comment) => (
+          <CommentItem comment={comment} />
+        ))} */}
+        {comment.length >= 5 ? <S.MoreButton>더보기</S.MoreButton> : ""}
+      </S.CommentWrapper>
+    </>
+  );
 };
 
 export default Comment;
