4c8401aa0dfa0fc000834f6245a3b9c5ea2cba33
강은빈 <kub0803@naver.com>
2021-09-26 23:18:32 +0900

    [ADD] Link 추가

--- src/components/portfolioList/list/ListItem.tsx
+++ src/components/portfolioList/list/ListItem.tsx
@@ -7,6 +7,9 @@ import {
 } from "../../../util/assets";
 import * as S from "./style";
 import { PortListType } from "../../../util/interface/main/portListType";
+import { Link } from "react-router-dom";
+
+interface Prop {}
 
 const Tag = ({ field }: any) => {
   return <S.Tag>{field}</S.Tag>;
@@ -20,6 +23,7 @@ const ListItem = ({
   introduce,
   touching,
   touched,
+  id,
 }: PortListType) => {
   const [touchingBoolean, setTouchingBoolean] = useState<boolean>(touched);
   const [count, setCount] = useState<number>(touching);
@@ -37,42 +41,44 @@ const ListItem = ({
   }
 
   return (
-    <S.ListItemWrapper>
-      <div className="portfoilo-img">
-        <img src={Flower} alt="포트폴리오 배너" />
-      </div>
-      <S.Content touchingBoolean={touchingBoolean}>
-        <div className="tag-wrapper">
-          <div className="tag">
-            {field.map((field, index) => (
-              <Tag key={index} field={field} />
-            ))}
+    <Link to={`portfolio/${id}`}>
+      <S.ListItemWrapper>
+        <div className="portfoilo-img">
+          <img src={Flower} alt="포트폴리오 배너" />
+        </div>
+        <S.Content touchingBoolean={touchingBoolean}>
+          <div className="tag-wrapper">
+            <div className="tag">
+              {field.map((field, index) => (
+                <Tag key={index} field={field} />
+              ))}
+            </div>
+            <div className="touching">
+              <img
+                src={touchingBoolean ? Touching : BeforeTouching}
+                alt="터칭 아이콘"
+                onClick={() => {
+                  console.log(touchingBoolean);
+                  setTouchingBoolean(!touchingBoolean);
+                  CountChangeHandler(count);
+                }}
+              />
+              <span>{count === 0 ? "" : count}</span>
+            </div>
           </div>
-          <div className="touching">
-            <img
-              src={touchingBoolean ? Touching : BeforeTouching}
-              alt="터칭 아이콘"
-              onClick={() => {
-                console.log(touchingBoolean);
-                setTouchingBoolean(!touchingBoolean);
-                CountChangeHandler(count);
-              }}
-            />
-            <span>{count === 0 ? "" : count}</span>
+          <div className="title">
+            <span>{title}</span>
+            <span>{TextSliceHandler(introduce, 32)}</span>
           </div>
-        </div>
-        <div className="title">
-          <span>{title}</span>
-          <span>{TextSliceHandler(introduce, 32)}</span>
-        </div>
-        <div className="user-profile">
-          <img src={Profile} alt="사용자의 프로필 사진" />
-          <span>
-            <strong>{user.name}</strong>님이 포트폴리오
-          </span>
-        </div>
-      </S.Content>
-    </S.ListItemWrapper>
+          <div className="user-profile">
+            <img src={Profile} alt="사용자의 프로필 사진" />
+            <span>
+              <strong>{user.name}</strong>님이 포트폴리오
+            </span>
+          </div>
+        </S.Content>
+      </S.ListItemWrapper>
+    </Link>
   );
 };
 
