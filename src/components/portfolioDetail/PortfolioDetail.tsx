4a88177953774ad77c757a4bdab140b6db93f4e4
강은빈 <kub0803@naver.com>
2021-09-26 11:12:44 +0900

    [ADD] 포트폴리오 상세 컴포넌트

---
+++ src/components/portfolioDetail/PortfolioDetail.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header, Comment } from "..";
+import * as S from "./style";
+
+const PortfolioDetail = () => {
+  return (
+    <>
+      <Header />
+      <S.DetailWrappper></S.DetailWrappper>
+      <Comment />
+    </>
+  );
+};
+
+export default PortfolioDetail;
