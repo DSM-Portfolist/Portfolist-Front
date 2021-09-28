952e488ddc9b3a8b65bd0c3435a0ffe272c0af9a
강은빈 <kub0803@naver.com>
2021-09-28 23:30:56 +0900

    [MODIFY] 포트폴리오 상세 페이지 수정

--- src/components/portfolioDetail/PortfolioDetail.tsx
+++ src/components/portfolioDetail/PortfolioDetail.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Header, Comment } from "..";
+import { Header, Comment, Title } from "..";
+import * as portfolio from "./dummy.json";
+
 import * as S from "./style";
 
 const PortfolioDetail = () => {
   return (
     <>
       <Header />
-      <S.DetailWrappper></S.DetailWrappper>
+      <S.DetailWrappper>
+        <Title portfolio={portfolio} />
+      </S.DetailWrappper>
       <Comment />
     </>
   );
