4cdb582edcdc3ef3211a0fc82e4b62fe4fb9201b
강은빈 <kub0803@naver.com>
2021-09-29 23:37:13 +0900

    [ADD] 추가정보 아이템 컴포넌트

---
+++ src/components/portfolioDetail/items/moreInfo/MoreInfoItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const MoreInfoItem = () => {
+  return (
+    <S.InfoItem>
+      <span>github</span>
+      <span>https://github.com/silverbeen</span>
+    </S.InfoItem>
+  );
+};
+export default MoreInfoItem;
---
+++ src/components/portfolioDetail/items/moreInfo/style.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const MoreInfoWrapper = styled.div`
+    .more-wrapper{
+        
+    }
+`;
+
+export const InfoItem = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
