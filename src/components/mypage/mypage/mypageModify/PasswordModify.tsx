bb26ee6091641250c0dc1325779435806ec0e594
JaeHyun <soehd0889@naver.com>
2021-10-06 15:20:46 +0900

    [ADD] 마이페이지 수정 비밀번호 수정 컨텐트 추가

---
+++ src/components/mypage/mypage/mypageModify/PasswordModify.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { ContentItem } from "../../../../util/css/mypage/mypage/mypageModify/style";
+
+const PasswordModify = () => {
+  return (
+    <div css={[ContentItem]}>
+      <h1>비밀번호 변경</h1>
+      <span>변경</span>
+      <p>새로운 비밀번호로 변경 할 수 있습니다.</p>
+    </div>
+  );
+};
+
+export default PasswordModify;
