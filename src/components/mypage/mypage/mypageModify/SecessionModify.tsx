eade269d625aeeefaaceeaa43aca3701e8c337c8
JaeHyun <soehd0889@naver.com>
2021-10-06 15:21:04 +0900

    [ADD] 마이페이지 수정 탈퇴 컴포넌트 추가

---
+++ src/components/mypage/mypage/mypageModify/SecessionModify.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { ContentItem } from "../../../../util/css/mypage/mypage/mypageModify/style";
+
+const SecessionModify = () => {
+  return (
+    <div css={[ContentItem]}>
+      <h1>portfolist 탈퇴</h1>
+      <span>탈퇴</span>
+      <p>portfolist 를 탈퇴하시면 이전에 활동했던 기록들은 복구가 불가능합니다.</p>
+    </div>
+  );
+};
+
+export default SecessionModify;
