5ad15445c712e921f08c47f7579b6b3bfd6d2488
JaeHyun <soehd0889@naver.com>
2021-10-06 16:14:11 +0900

    [ADD] 탈퇴 버튼 토글기능 추가

--- src/components/mypage/mypage/mypageModify/SecessionModify.tsx
+++ src/components/mypage/mypage/mypageModify/SecessionModify.tsx
@@ -1,14 +1,25 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import { ContentItem } from "../../../../util/css/mypage/mypage/mypageModify/style";
 
 const SecessionModify = () => {
+  const [isClickSecessionButton, setIsClickSecessionButton] =
+    useState<boolean>(false);
+  const toggleButton = () => {
+    isClickSecessionButton
+      ? setIsClickSecessionButton(false)
+      : setIsClickSecessionButton(true);
+  };
   return (
-    <div css={[ContentItem]}>
-      <h1>portfolist 탈퇴</h1>
-      <span>탈퇴</span>
-      <p>portfolist 를 탈퇴하시면 이전에 활동했던 기록들은 복구가 불가능합니다.</p>
-    </div>
+    <>
+      <div css={[ContentItem]}>
+        <h1>portfolist 탈퇴</h1>
+        <span onClick={toggleButton}>탈퇴</span>
+        <p>
+          portfolist 를 탈퇴하시면 이전에 활동했던 기록들은 복구가 불가능합니다.
+        </p>
+      </div>
+    </>
   );
 };
 
