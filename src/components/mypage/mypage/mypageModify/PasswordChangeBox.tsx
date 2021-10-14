f7ba8e652e9a2d8eb31eefabbd68b4eef3e24784
JaeHyun <soehd0889@naver.com>
2021-10-14 20:11:21 +0900

    [ADD] 비밀번호 onChange 함수 추가, 문구 추가

--- src/components/mypage/mypage/mypageModify/PasswordChangeBox.tsx
+++ src/components/mypage/mypage/mypageModify/PasswordChangeBox.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { CloseEye, OpenEye } from "../../../../util/assets";
-import { ChangePasswordItem } from "../../../../util/css/mypage/mypage/mypageModify/style";
+import {
+  ChangePasswordItem,
+  PasswordChangeWrapper,
+} from "../../../../util/css/mypage/mypage/mypageModify/style";
 
 interface Props {
   title: string;
   passwordType: any;
   visible: any;
   value: number;
+  handleOnChangeEvent: (e: any, value: number) => void;
   handlePasswordType: (value: number) => void;
 }
 
 const PasswordChangeBox = (props: Props) => {
   return (
-    <div>
+    <PasswordChangeWrapper>
       <ChangePasswordItem>
         <h1>{props.title}</h1>
-        <input type={props.passwordType} />
+        <input
+          type={props.passwordType}
+          onChange={(e) => {
+            props.handleOnChangeEvent(e, props.value);
+          }}
+        />
         <img
           onClick={() => {
             props.handlePasswordType(props.value);
@@ -24,7 +33,8 @@ const PasswordChangeBox = (props: Props) => {
           alt=""
         />
       </ChangePasswordItem>
-    </div>
+      {props.value == 2 ? <span>비밀번호는 4글자 이상 입력해주세요.</span> : ""}
+    </PasswordChangeWrapper>
   );
 };
 
