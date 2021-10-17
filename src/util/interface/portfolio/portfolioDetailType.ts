aa6be86b4d61c069493034651078a7eb9f21f719
강은빈 <kub0803@naver.com>
2021-10-17 23:34:00 +0900

    [MODIFY] 폴더 구조 변경

---
+++ src/util/interface/portfolio/portfolioDetailType.ts
@@ -0,0 +1,42 @@
+import { CommentType } from "./commentType";
+import { UserType } from "../main/userType";
+
+export interface PortfolioType {
+  default?: any;
+  user: UserType;
+  create_date: string;
+  touched: boolean;
+  total_touching: number;
+  icon: string;
+  title: string;
+  introduce: string;
+  field: string[];
+  more_info: MoreInfoType[];
+  container_list: ContainerListType[];
+  certificate: string[];
+  link: string;
+  file: string;
+  comments: CommentType[];
+}
+
+//추가 정보 타입
+export interface MoreInfoType {
+  key: string;
+  value: string;
+}
+
+export interface ContainerListType {
+  container_title: string;
+  container: ContainerType[];
+}
+
+export interface ContainerType {
+  map: any;
+  box_img: string[];
+  box_list: BoxListType[];
+}
+
+export interface BoxListType {
+  box_title: string;
+  box_content: string;
+}
