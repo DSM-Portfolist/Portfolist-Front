08821cb8ac81a9f5ff2935cd63b4cf579ac1ea5e
강은빈 <kub0803@naver.com>
2021-10-17 23:40:41 +0900

    [MODIFY] 포트폴리오 작성 UI 수정

--- src/components/portfolioDetail/style.ts
+++ src/components/portfolioDetail/style.ts
@@ -3,10 +3,13 @@ import styled from "@emotion/styled";
 export const DetailWrappper = styled.div`
   width: 63%;
   min-width: 760px;
-  height: 1000px;
   border-left: 2px solid #f0f0f0;
   border-right: 2px solid #f0f0f0;
   margin: 0 auto;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 
   .portfolio-container {
     width: 86%;
