2e5a1ea6a16d2f4dbb91148e02925efccf197be9
강은빈 <kub0803@naver.com>
2021-10-18 19:39:39 +0900

    [MODIFY] 구조 변경

--- src/components/portfolioDetail/items/experience/ExperienceList.tsx
+++ src/components/portfolioDetail/items/experience/ExperienceList.tsx
@@ -1,39 +1,38 @@
 import React from "react";
 import { TestImg } from "../../../../util/assets";
 import {
+  BoxListType,
   ContainerListType,
   ContainerType,
-  PortfolioType,
 } from "../../../../util/interface/portfolio/portfolioDetailType";
 import * as S from "./style";
+import { container_list } from "../../dummy.json";
 
-interface Props {
-  portfolio: ContainerType;
-}
-
-const ExperienceList = ({ portfolio }: Props) => {
-  console.log(portfolio);
+const ExperienceList = () => {
+  const portfolio: ContainerListType[] = container_list;
 
   return (
     <>
       {portfolio.map((item: ContainerListType, index: number) => (
         <S.ExperienceWrapper key={index}>
           <S.MainTitle>{item.container_title}</S.MainTitle>
-          <S.ContainerBox>
-            <img src={TestImg} alt="경험 이미지" />
-            <S.ContentWraper>
-              <span className="sub-title">
-                🥈 2021 지방기능경기대회 웹디자인 및 개발 은메달
-              </span>
-              <span className="sub-content">
-                지방기능경기대회를 준비하면서 지방기능경기대회를 준비하면서
-                지방기능경기대회를 준비 지방기능경기대회를 준비하면서
-                지방기능경기대회를 준비하면서 지방기능경기대회를
-                준비하면지방기능경기대회를 준비하면서 지방기능경기대회를
-                준비하면서 지방기능경기대회를 준비하면서 열심히 했습니다.
-              </span>
-            </S.ContentWraper>
-          </S.ContainerBox>
+          {item.container.map((list: ContainerType, index: number) => (
+            <S.ContainerBox key={index}>
+              <div className="image-box">
+                {list.box_img.map((img: string, index: number) => (
+                  <img key={index} src={TestImg} alt="경험 이미지" />
+                ))}
+              </div>
+              <div className="content-box">
+                {list.box_list.map((box: BoxListType, index: number) => (
+                  <div key={index} className="content-box-item">
+                    <span className="sub-title">{box.box_title}</span>
+                    <span className="sub-content">{box.box_content}</span>
+                  </div>
+                ))}
+              </div>
+            </S.ContainerBox>
+          ))}
         </S.ExperienceWrapper>
       ))}
     </>
