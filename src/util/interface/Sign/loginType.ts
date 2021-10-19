74e553a66e9a4043c90bc80f4cec04e491271d44
강은빈 <kub0803@naver.com>
2021-10-19 15:20:10 +0900

    [ADD] 회원가입 타입 추가

--- src/util/interface/Sign/loginType.ts
+++ src/util/interface/Sign/loginType.ts
@@ -2,3 +2,10 @@ export interface BorderProp {
   passwordBor: boolean;
   emailBor: boolean;
 }
+
+export interface SignUpType {
+  name: string;
+  email: string;
+  password: string;
+  field: number[];
+}
