04ea2fc071c39843e10ee45b5f768b75f1b9f9fd
강은빈 <kub0803@naver.com>
2021-10-19 15:21:10 +0900

    [ADD] 회원가입 input 정리

--- src/components/signin/signUp/SignUp.tsx
+++ src/components/signin/signUp/SignUp.tsx
@@ -1,12 +1,21 @@
 /** @jsxImportSource @emotion/react */
 
+import { useState } from "react";
 import { baseBackground, center } from "../../../util/css/signin/LoginEmotion";
+import { SignUpType } from "../../../util/interface/Sign/loginType";
 import FirstProgress from "./process/FirstProgress";
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState<SignUpType>({
+    name: "",
+    email: "",
+    password: "",
+    field: [],
+  });
+
   return (
     <section css={[baseBackground, center]}>
-      <FirstProgress />
+      <FirstProgress inputs={inputs} setInputs={setInputs} />
     </section>
   );
 };
