ee680f10b8c0998706e1fb1c759d3d7bfc6cc451
JaeHyun <soehd0889@naver.com>
2021-10-21 18:17:31 +0900

    [ADD] ReactQuery 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import UserRouter from "./router/UserRouter";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <UserRouter />
+      <QueryClientProvider client={queryClient}>
+        <UserRouter />
+      </QueryClientProvider>
     </>
   );
 }
