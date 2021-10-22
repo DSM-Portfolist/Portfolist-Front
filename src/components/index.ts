c11f35c375e7422bf133fa07204e416528afe8bf
강은빈 <kub0803@naver.com>
2021-10-22 13:12:46 +0900

    [MODIFY] 폴더 구조 변경

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export { default as SignUp } from "./signin/signUp/SignUp";
 export { default as FirstProgress } from "./signin/signUp/process/FirstProgress";
 export { default as SecProgress } from "./signin/signUp/process/SecProgress";
 export { default as Main } from "./main/Main/Main";
-export { default as Header } from "./main/header/Header";
+export { default as Header } from "./commom/header/Header";
 export { default as Banner } from "./main/Main/Banner";
 export { default as List } from "./main/Main/portList/List";
 export { default as MonthPortfolio } from "./main/Main/monthPortfolio/MonthPortfolio";
