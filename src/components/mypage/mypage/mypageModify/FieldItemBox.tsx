4f5f9b17493d7a004c81779d543d346121ea4ac0
JaeHyun <soehd0889@naver.com>
2021-10-22 18:48:07 +0900

    [ADD] 서버에 보내는 ID 배열 삭제 기능 추가

--- src/components/mypage/mypage/mypageModify/FieldItemBox.tsx
+++ src/components/mypage/mypage/mypageModify/FieldItemBox.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "../../../../util/css/mypage/mypage/mypageModify/style";
 import { CloseIcon } from "../../../../util/assets";
 
+type fieldItemType = {
+  id: string;
+  content: string;
+};
+
 interface Props {
+  selectNameArr?: string[];
+  setSelectIdArr: any;
+  setSelectNameArr: any;
+  selectIdArr?: string[];
+  fieldItem: Array<fieldItemType>;
   field: string;
-  setFieldList?: any;
-  fieldList?: [];
 }
 
 const FieldItemBox = (props: Props) => {
-  const {} = Props;
+  const {
+    selectNameArr,
+    selectIdArr,
+    setSelectIdArr,
+    setSelectNameArr,
+    fieldItem,
+    field,
+  } = props;
+
+  function UseFieldDelete(txt: string, id: string) {
+    let newArr = selectIdArr;
+    newArr?.splice(newArr.indexOf(`${id}`), 1);
+    setSelectNameArr(selectNameArr?.filter((field: any) => field !== txt));
+    setSelectIdArr(newArr);
+  }
+
   return (
     <S.FieldItemContainer>
       <span>{field}</span>
@@ -17,7 +40,14 @@ const FieldItemBox = (props: Props) => {
         src={CloseIcon}
         alt="닫기 아이콘"
         onClick={() => {
-          UseFieldDelete(field);
+          let index = 0;
+          fieldItem.forEach((e) => {
+            if (field === e.content) {
+              return;
+            }
+            index += 1;
+          });
+          UseFieldDelete(field, fieldItem[index].id);
         }}
       />
     </S.FieldItemContainer>
