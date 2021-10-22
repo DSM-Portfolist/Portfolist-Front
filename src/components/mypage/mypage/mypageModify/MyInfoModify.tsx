bf85af6b0f83fd8a5732087dfdbc5ac7437a6c61
JaeHyun <soehd0889@naver.com>
2021-10-22 20:04:03 +0900

    [ADD] 마이페이지 정보 수정 취소 버튼 함수 props 추가

--- src/components/mypage/mypage/mypageModify/MyInfoModify.tsx
+++ src/components/mypage/mypage/mypageModify/MyInfoModify.tsx
@@ -29,7 +29,7 @@ const MyInfoModify = () => {
           />
         </MainWrapper>
       ) : (
-        <MyInfoModifyDetail />
+        <MyInfoModifyDetail setIsModify={setIsModify} />
       )}
     </>
   );
