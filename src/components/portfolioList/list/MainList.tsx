6aee789124e766ec285e35e503600094d38f6657
강은빈 <kub0803@naver.com>
2021-10-24 00:16:10 +0900

    [FEAT] 포트폴리오 리스트 API

--- src/components/portfolioList/list/MainList.tsx
+++ src/components/portfolioList/list/MainList.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import * as S from "./style";
 import { list } from "./dummy.json";
 import { Search, ListItem } from "../..";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { MAINURL } from "../../../util/api/index";
 
 const MainList = () => {
+  const { data } = useQuery("get-list", () =>
+    axios(`${MAINURL}/portfolio/list?page=1&size=5&field=WEB`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "access_token_portfolist"
+        )}`,
+      },
+    })
+  );
+
+  console.log(data);
+
   return (
     <>
       <S.MainListWrapper className="container">
