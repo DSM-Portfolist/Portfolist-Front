ad835e79ffebe3e6b6c78370cc1e5164928c2427
강은빈 <kub0803@naver.com>
2021-10-25 23:04:01 +0900

    [FEAT] 댓글 검색 결과

--- src/components/portfolioList/list/MainList.tsx
+++ src/components/portfolioList/list/MainList.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import * as S from "./style";
-import { list } from "./dummy.json";
 import { Search, ListItem } from "../..";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import axios from "axios";
 import { MAINURL } from "../../../util/api/index";
+import { PortListType } from "../../../util/interface/portfolio/portListType";
 
-const MainList = () => {
-  const { data } = useQuery("get-list", () =>
+interface Props {
+  searchValue: string;
+  setSearchValue: any;
+}
+
+const MainList = ({ searchValue, setSearchValue }: Props) => {
+  
+  const { isLoading, data } = useQuery("get-list", () =>
     axios(`${MAINURL}/portfolio/list?page=1&size=5&field=WEB`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem(
@@ -17,16 +23,32 @@ const MainList = () => {
     })
   );
 
-  console.log(data);
-
   return (
     <>
       <S.MainListWrapper className="container">
-        <Search />
+        <Search setSearchValue={setSearchValue} />
+
         <S.ListWrapper>
-          {list.map((list: any, index: number) => (
-            <ListItem key={index} list={list} />
-          ))}
+          {isLoading ? (
+            <div>잠시만 기다려주세요</div>
+          ) : (
+            <>
+              {/*  검색결과가 없으면 */}
+              {searchValue.length === 0 ? (
+                ""
+              ) : (
+                <S.SearchContent>{searchValue}의 검색결과...</S.SearchContent>
+              )}
+              {/* 리스트 띄우기 */}
+              <S.ListContent>
+                {data?.data.portfolio_list.map(
+                  (list: PortListType, index: number) => (
+                    <ListItem key={index} list={list} />
+                  )
+                )}
+              </S.ListContent>
+            </>
+          )}
         </S.ListWrapper>
       </S.MainListWrapper>
     </>
