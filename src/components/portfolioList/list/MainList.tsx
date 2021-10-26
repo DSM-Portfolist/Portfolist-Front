674276a82edf9b07acd7c0d70bddf80d7f741036
강은빈 <kub0803@naver.com>
2021-10-26 23:42:59 +0900

    [REFACTOR] 코드 정리

--- src/components/portfolioList/list/MainList.tsx
+++ src/components/portfolioList/list/MainList.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import * as S from "./style";
 import { Search, ListItem } from "../..";
-import { useMutation, useQuery } from "react-query";
-import axios from "axios";
-import { MAINURL } from "../../../util/api/index";
 import { PortListType } from "../../../util/interface/portfolio/portListType";
+import { useList } from "../../../util/api/portfolio/getList";
 
 interface Props {
   searchValue: string;
@@ -12,45 +10,31 @@ interface Props {
 }
 
 const MainList = ({ searchValue, setSearchValue }: Props) => {
-  const { isLoading, data } = useQuery("get-list", () =>
-    axios(`${MAINURL}/portfolio/list?page=1&size=5&field=WEB`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem(
-          "access_token_portfolist"
-        )}`,
-      },
-    })
-  );
+  const { isLoading, data } = useList();
 
   return (
-    <>
-      <S.MainListWrapper className="container">
-        <Search setSearchValue={setSearchValue} />
+    <S.MainListWrapper className="container">
+      <Search setSearchValue={setSearchValue} />
+      <S.ListWrapper>
+        {isLoading ? (
+          <div>잠시만 기다려주세요</div>
+        ) : (
+          <>
+            {searchValue.length === 0 ? (
+              ""
+            ) : (
+              <S.SearchContent>{searchValue}의 검색결과...</S.SearchContent>
+            )}
 
-        <S.ListWrapper>
-          {isLoading ? (
-            <div>잠시만 기다려주세요</div>
-          ) : (
-            <>
-              {/*  검색결과가 없으면 */}
-              {searchValue.length === 0 ? (
-                ""
-              ) : (
-                <S.SearchContent>{searchValue}의 검색결과...</S.SearchContent>
-              )}
-              {/* 리스트 띄우기 */}
-              <S.ListContent>
-                {data?.data.portfolio_list.map(
-                  (list: PortListType, index: number) => (
-                    <ListItem key={index} list={list} />
-                  )
-                )}
-              </S.ListContent>
-            </>
-          )}
-        </S.ListWrapper>
-      </S.MainListWrapper>
-    </>
+            <S.ListContent>
+              {data?.portfolio_list.map((list: PortListType, index: number) => (
+                <ListItem key={index} list={list} />
+              ))}
+            </S.ListContent>
+          </>
+        )}
+      </S.ListWrapper>
+    </S.MainListWrapper>
   );
 };
 
