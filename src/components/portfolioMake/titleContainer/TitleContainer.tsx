ae3bff2eb267423b224b3dff90a80f2637817080
JaeHyun <soehd0889@naver.com>
2021-10-28 08:30:12 +0900

    [ADD] 포트폴리오 제작 컴포넌트 글작성 check 함수 추가

--- src/components/portfolioMake/titleContainer/TitleContainer.tsx
+++ src/components/portfolioMake/titleContainer/TitleContainer.tsx
@@ -1,19 +1,38 @@
 import React, { useState } from "react";
 import * as S from "./style";
+import { InputIsNullType } from "../../../util/interface/portfolio/portfolioMakeType";
 
 const TitleContainer = () => {
-  const [isNullArr, setIsNullArr] = useState<any>([
+  const [isNullArr, setIsNullArr] = useState<InputIsNullType[]>([
     { isNull: true },
     { isNull: true },
   ]);
 
   const handlerOnChange = (e: any) => {
+    //글작성을 했는지 안했는지 확인하는 함수
+    isNullCheckFunc(e);
+  };
+
+  const isNullCheckFunc = (e: any) => {
+    //글작성을 했는지 안했는지 확인하는 함수
     const { className, value } = e.target;
-    console.log(e);
     if (value !== "") {
-      let newArr = isNullArr.map((item: any) => {
-        if (className === "Title" || className === "Introduce") {
-          return { isNullArr: false };
+      let newArr = isNullArr.map((item: any, index: number) => {
+        console.log(item, index);
+        if (className === "Title" && index === 0) {
+          return { isNull: false };
+        } else if (className === "Introduce" && index === 1) {
+          return { isNull: false };
+        }
+        return item;
+      });
+      setIsNullArr(newArr);
+    } else {
+      let newArr = isNullArr.map((item: any, index: number) => {
+        if (className === "Title" && index === 0) {
+          return { isNull: true };
+        } else if (className === "Introduce" && index === 1) {
+          return { isNull: true };
         }
         return item;
       });
@@ -29,15 +48,15 @@ const TitleContainer = () => {
           className="Title"
           placeholder="제목을 입력해주세요."
         />
-        {isNullArr[0] ? <span className="title-span">*</span> : ""}
+        {isNullArr[0].isNull ? <span className="title-span">*</span> : ""}
       </S.InputWrapper>
-      <S.InputWrapper>
+      <S.InputWrapper onChange={handlerOnChange}>
         <input
           type="text"
           className="Introduce"
           placeholder="본인을 간단하게 소개해주세요."
         />
-        {isNullArr[1] ? <span className="introduce-span">*</span> : ""}
+        {isNullArr[1].isNull ? <span className="introduce-span">*</span> : ""}
       </S.InputWrapper>
     </S.MainContainer>
   );
