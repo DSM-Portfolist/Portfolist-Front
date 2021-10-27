464aee8a85a4b0aaea63cdc244f32d834bf7a174
JaeHyun <soehd0889@naver.com>
2021-10-27 16:18:26 +0900

    [ADD] STYLE

---
+++ src/components/portfolioMake/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  width: 63%;
+  min-width: 760px;
+  border-left: 2px solid #f0f0f0;
+  border-right: 2px solid #f0f0f0;
+  margin: 0 auto;
+`;
