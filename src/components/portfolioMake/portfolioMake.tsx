8517f17ee4a87717c901acd4a78f5993d3cba945
JaeHyun <soehd0889@naver.com>
2021-10-27 23:52:34 +0900

    [ADD] 주의 사항 추가

--- src/components/portfolioMake/portfolioMake.tsx
+++ src/components/portfolioMake/portfolioMake.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Header } from "..";
 import * as S from "./style";
+import TitleContainer from "./titleContainer/TitleContainer";
 
 const portfolioMake = () => {
   return (
     <>
       <Header />
+      <S.MainContainer>
+        <p className="caution">
+          파일 혹은 링크 첨부로 포트폴리오를 업로드 하실 경우 제목과 본인
+          소개를필수로 작성해주세요.
+        </p>
+        <TitleContainer />
+      </S.MainContainer>
     </>
   );
 };
