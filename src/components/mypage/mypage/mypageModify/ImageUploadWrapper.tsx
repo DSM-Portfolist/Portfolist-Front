690935d19ffc8abb0b68cf6d284d5e22701d1fd9
JaeHyun <soehd0889@naver.com>
2021-10-27 16:18:44 +0900

    [ADD] FormData로 가공하기

--- src/components/mypage/mypage/mypageModify/ImageUploadWrapper.tsx
+++ src/components/mypage/mypage/mypageModify/ImageUploadWrapper.tsx
@@ -1,12 +1,25 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProfileImageWrapper } from "../../../../util/css/mypage/mypage/mypageModify/style";
 import { profileImage } from "../../../../util/css/mypage/ProfileHeader/style";
 
 const ImageUploadWrapper = () => {
-  const [imageFile, setImageFile] = useState<any>("");
+  const [imageFile, setImageFile] = useState<any>([]);
   const [previewURL, setPreviewURL] = useState<any>("");
   const [isCustomImage, setIsCustomImage] = useState<boolean>(false);
+
+  let formData = new FormData();
+
+  function testAsd() {
+    //FormData 값 확인하기
+    for (let key of formData.keys()) {
+      console.log(key);
+    }
+    // FormData의 value 확인
+    for (let value of formData.values()) {
+      console.log(value);
+    }
+  }
 
   const handleFileOnChange = (e: any) => {
     //이미지 파일 브리뷰
@@ -16,7 +29,9 @@ const ImageUploadWrapper = () => {
     reader.onloadend = () => {
       setImageFile(file);
       setPreviewURL(reader.result);
+      formData.append("file", file);
       setIsCustomImage(true);
+      testAsd();
     };
     reader.readAsDataURL(file);
   };
