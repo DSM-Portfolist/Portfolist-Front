af58aeb90b045df1f4817c0d1fa62cbd180e2401
강은빈 <kub0803@naver.com>
2021-10-28 17:27:03 +0900

    [FEAT] portfoilo api

--- src/components/portfolioDetail/PortfolioDetail.tsx
+++ src/components/portfolioDetail/PortfolioDetail.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import { useSetRecoilState } from "recoil";
 import { Header, Comment, Title, ExperienceList } from "..";
-import * as portfolio from "./dummy.json";
+import { portfoilo } from "../../modules/atom/portfolio";
+import { getPortfolist } from "../../util/api/portfolio/getList";
 import MoreInfo from "./items/moreInfo/MoreInfo";
 import * as S from "./style";
 
 const PortfolioDetail = () => {
+  const { data, isLoading, error } = useQuery("getPortfolio", getPortfolist);
+  const setPortfolioValue = useSetRecoilState(portfoilo);
+
+  useEffect(() => {
+    setPortfolioValue(data?.data);
+  }, [data?.data, setPortfolioValue]);
+
+  if (isLoading) return <>잠시만 기다려주세요</>;
+  if (error) return <>404 서버 오류입니다. </>;
+
   return (
     <>
       <Header />
       <S.DetailWrappper>
         <div className="portfolio-container">
-          <Title portfolio={portfolio} />
-          <MoreInfo portfolio={portfolio} />
+          <Title />
+          <MoreInfo />
           <ExperienceList />
         </div>
       </S.DetailWrappper>
