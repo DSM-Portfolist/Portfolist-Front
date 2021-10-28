6ffb2d42cfec61c23004f6d80f2fb03a724a432b
강은빈 <kub0803@naver.com>
2021-10-28 17:17:51 +0900

    [FEAT] API 연동

--- src/components/portfolioDetail/items/Title/Title.tsx
+++ src/components/portfolioDetail/items/Title/Title.tsx
@@ -1,42 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { portfoilo } from "../../../../modules/atom/portfolio";
 import { Flower } from "../../../../util/assets";
-import { PortfolioType } from "../../../../util/interface/portfolio/portfolioDetailType";
 import * as S from "./style";
 
-interface Props {
-  portfolio: PortfolioType;
-}
-
 const FieldItem = (field: any) => {
-  console.log(field.field);
   return <S.FieldItemWrapper>{field.field}</S.FieldItemWrapper>;
 };
 
-const Title = ({ portfolio }: Props) => {
-  const list: PortfolioType = portfolio.default;
+const Title = () => {
+  const portfolioValue = useRecoilValue(portfoilo);
+
+  useEffect(() => {
+    console.log(portfolioValue);
+  }, [portfolioValue]);
 
   return (
     <S.TitleWrapper>
       <S.TitleInfo>
         <S.FieldWrapper>
-          {list.field.map((field, index) => {
+          {portfolioValue?.field.map((field: string, index: number) => {
             return <FieldItem key={index} field={field} />;
           })}
         </S.FieldWrapper>
         <S.DateWrapper>
-          <span>게시일 2020년 8월 9일</span>
+          <span>{portfolioValue?.create_date}</span>
           <div className="user-profile">
-            <span>금은동빈빈빈빈빈빈빈</span>
+            <span>{portfolioValue?.name}</span>
             <img src={Flower} alt="유저 프로필 이미지" />
           </div>
         </S.DateWrapper>
       </S.TitleInfo>
       <S.HeadTitle>
         <span role="img">&#127802;</span>
-        <span className="title">강은빈 (Kang Eun Bin)</span>
-        <span className="sub-title">
-          금처럼 반짝반짝 빛나게 될 강은빈입니다.
-        </span>
+        <span className="title">{portfolioValue?.title}</span>
+        <span className="sub-title">{portfolioValue?.introduce}</span>
       </S.HeadTitle>
     </S.TitleWrapper>
   );
