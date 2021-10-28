979b5e47d2a8cdff7433593e12d6bf4ea48e0272
강은빈 <kub0803@naver.com>
2021-10-29 00:42:13 +0900

    [ADD] header 추가

--- src/util/api/index.ts
+++ src/util/api/index.ts
@@ -2,3 +2,5 @@ export const MAINURL = process.env.REACT_APP_API_URL;
 export const token = `Bearer ${localStorage.getItem(
   "access_token_portfolist"
 )}`;
+
+export const header = ` { Authorization: token },`;
