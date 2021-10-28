cd1d59a493902a701fb4202a3087df723ec4e51c
JaeHyun <soehd0889@naver.com>
2021-10-28 23:56:59 +0900

    [MODIFY] 주석 및 수정

--- src/components/portfolioMake/portfolioMake.tsx
+++ src/components/portfolioMake/portfolioMake.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Header } from "..";
 import * as S from "./style";
 import TitleContainer from "./titleContainer/TitleContainer";
+import MoreInfoContainer from "./moreInfoContainer/MoreInfoContainer";
 
 const portfolioMake = () => {
   return (
     <>
       <Header />
       <S.MainContainer>
-        <p className="caution">
-          파일 혹은 링크 첨부로 포트폴리오를 업로드 하실 경우 제목과 본인
-          소개를필수로 작성해주세요.
-        </p>
+        <p className="caution">파일 혹은 링크 첨부로 포트폴리오를 업로드하실 경우 제목과 본인 소개를 필수로 작성해 주세요.</p>
         <TitleContainer /> {/* 제목 컴포넌트 */}
+        <MoreInfoContainer />{/*이메일이나 깃허브 넣는 컴포넌트*/}
       </S.MainContainer>
     </>
   );
