799c8c4897c2a81883b7bd70e7b278ddbb4d6027
강은빈 <kub0803@naver.com>
2021-10-28 17:26:50 +0900

    [FEAT] container_list api

--- src/components/portfolioDetail/items/experience/ExperienceList.tsx
+++ src/components/portfolioDetail/items/experience/ExperienceList.tsx
@@ -1,30 +1,32 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
 import * as S from "./style";
 import { TestImg } from "../../../../util/assets";
 import {
   BoxListType,
   ContainerListType,
-  ContainerType,
 } from "../../../../util/interface/portfolio/portfolioDetailType";
-import { container_list } from "../../dummy.json";
+import { portfoilo } from "../../../../modules/atom/portfolio";
 
 const ExperienceList = () => {
-  const portfolio: ContainerListType[] = container_list;
+  const portfolioValue = useRecoilValue(portfoilo);
+
+  console.log(portfolioValue?.container_list);
 
   return (
     <>
-      {portfolio.map((item: ContainerListType, index: number) => (
-        <S.ExperienceWrapper key={index}>
-          <S.MainTitle>{item.container_title}</S.MainTitle>
-          {item.container.map((list: ContainerType, index: number) => (
+      {portfolioValue?.container_list.map(
+        (item: ContainerListType, index: number) => (
+          <S.ExperienceWrapper key={index}>
+            <S.MainTitle>{item.container_title}</S.MainTitle>
             <S.ContainerBox key={index}>
               <div className="image-box">
-                {list.box_img.map((img: string, index: number) => (
+                {item.box_img.map((img: string, index: number) => (
                   <img key={index} src={TestImg} alt="경험 이미지" />
                 ))}
               </div>
               <div className="content-box">
-                {list.box_list.map((box: BoxListType, index: number) => (
+                {item.box_list.map((box: BoxListType, index: number) => (
                   <div key={index} className="content-box-item">
                     <span className="sub-title">{box.box_title}</span>
                     <span className="sub-content">{box.box_content}</span>
@@ -32,9 +34,9 @@ const ExperienceList = () => {
                 ))}
               </div>
             </S.ContainerBox>
-          ))}
-        </S.ExperienceWrapper>
-      ))}
+          </S.ExperienceWrapper>
+        )
+      )}
     </>
   );
 };
--- src/components/portfolioDetail/style.ts
+++ src/components/portfolioDetail/style.ts
@@ -3,9 +3,7 @@ import styled from "@emotion/styled";
 export const DetailWrappper = styled.div`
   width: 63%;
   min-width: 760px;
-  border-left: 2px solid #f0f0f0;
-  border-right: 2px solid #f0f0f0;
-  margin: 0 auto;
+  margin: 80px auto;
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
