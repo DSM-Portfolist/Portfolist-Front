2f8bc1ccd1c87bab5ea3116ced70cb63a548b150
강은빈 <kub0803@naver.com>
2021-10-29 23:50:55 +0900

    [MODIFY] url 수정

--- src/util/api/portfolio/getList.ts
+++ src/util/api/portfolio/getList.ts
@@ -3,7 +3,7 @@ import { MAINURL, token } from "..";
 
 export const getList = async () => {
   const { data } = await axios.get(
-    `${MAINURL}/portfolio/list?page=1&size=5&field=WEB`,
+    `${MAINURL}/portfolio/list?page=1&size=5&field=`,
     {
       headers: { Authorization: token },
     }
