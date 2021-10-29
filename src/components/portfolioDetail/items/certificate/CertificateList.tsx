d6f7946774710da7c3acae0d4d517781e660f63c
강은빈 <kub0803@naver.com>
2021-10-29 23:50:22 +0900

    [ADD] 자격증 컨테이너 추가

---
+++ src/components/portfolioDetail/items/certificate/CertificateList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./style";
+
+const CertificateList = () => {
+  return (
+    <S.Wrap>
+      <h1>제목입니다</h1>
+      <span>글글글글글글글글글글글글글글</span>
+    </S.Wrap>
+  );
+};
+
+export default CertificateList;
