1692359dc0cb9acbb9be20902080a4ee85d1401c
강은빈 <kub0803@naver.com>
2021-10-30 22:27:38 +0900

    [DELETE]

--- src/util/api/index.ts
+++ src/util/api/index.ts
@@ -2,5 +2,3 @@ export const MAINURL = process.env.REACT_APP_API_URL;
 export const token = `Bearer ${localStorage.getItem(
   "access_token_portfolist"
 )}`;
-
-export const header = ` { Authorization: token },`;
