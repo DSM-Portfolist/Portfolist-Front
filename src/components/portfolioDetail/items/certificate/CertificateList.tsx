af6df81c8d6498c76fab0c4dc1204638eaeceb31
강은빈 <kub0803@naver.com>
2021-11-02 23:49:52 +0900

    [MODIFY] key 값 변경

--- src/components/portfolioDetail/items/certificate/CertificateList.tsx
+++ src/components/portfolioDetail/items/certificate/CertificateList.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
-import { portfoilo } from "../../../../modules/atom/portfolio/portfolioDetail";
+import { getPortfolioSelecor } from "../../../../modules/atom/portfolio/portfolioDetail";
 import * as S from "./style";
 
 const CertificateList = () => {
-  const portfolioValue = useRecoilValue(portfoilo);
+  const portfolioValue = useRecoilValue(getPortfolioSelecor);
 
   return (
     <>
       {portfolioValue?.certificate_container_list.map((item, index) => (
-        <S.Wrap key="index">
+        <S.Wrap key={index}>
           <h1>{item?.title}</h1>
           {item.certificate_list.map((text, index) => (
             <span key={index}>{text}</span>
