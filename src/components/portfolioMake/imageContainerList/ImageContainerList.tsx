62d89600743ac1f994b4e3fc15d51708866ff51d
JaeHyun <soehd0889@naver.com>
2021-11-03 16:08:42 +0900

    [ADD] 포트폴리오 제작 이미지 리스트 컴포넌트 퍼블리싱

--- src/components/portfolioMake/imageContainerList/ImageContainerList.tsx
+++ src/components/portfolioMake/imageContainerList/ImageContainerList.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import * as S from "./style";
 
 const ImageContainerList = () => {
   return (
-    <div>
-      
-    </div>
+    <S.MainWrapper className="make-container">
+      <header>
+        <button type="button">리스트 추가하기</button>
+      </header>
+      <S.ImageWrapeerList>
+        <input
+          type="text"
+          className="Title"
+          placeholder="자신만의 특별한 경험(경력, 인턴, 교육 등)이 있다면 입력해주세요."
+        />
+        <S.ImageListSection>
+          <S.ImageWrapper></S.ImageWrapper>
+          <S.ContentWrapper></S.ContentWrapper>
+        </S.ImageListSection>
+      </S.ImageWrapeerList>
+    </S.MainWrapper>
   );
 };
 
