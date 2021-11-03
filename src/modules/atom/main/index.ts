f67004000be859075d53120221ae0c2ebf4128f0
강은빈 <kub0803@naver.com>
2021-11-03 23:48:35 +0900

    [ADD] 최근 포트폴리오 selector

--- src/modules/atom/main/index.ts
+++ src/modules/atom/main/index.ts
@@ -1,10 +1,21 @@
 import { selector } from "recoil";
-import { getMonthPortfolio } from "../../../util/api/mainpage";
+import {
+  getMonthPortfolio,
+  getRecentPortfolio,
+} from "../../../util/api/mainpage";
 
 export const monthPortfolioSelector = selector({
   key: "monthPortfolio/get",
   get: async () => {
     const res = await getMonthPortfolio();
+    return res.data;
+  },
+});
+
+export const recentPortfolioSelector = selector({
+  key: "recentPortfolio/get",
+  get: async () => {
+    const res = await getRecentPortfolio();
     return res.data;
   },
 });
