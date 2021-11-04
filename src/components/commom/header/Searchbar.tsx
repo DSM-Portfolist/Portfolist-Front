e73c7776898e5f1cdd23657dca28d5a13261d38a
강은빈 <kub0803@naver.com>
2021-11-04 17:10:53 +0900

    [MODIFY] 검색 값 atom 설정

--- src/components/commom/header/Searchbar.tsx
+++ src/components/commom/header/Searchbar.tsx
@@ -1,10 +1,20 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useSetRecoilState } from "recoil";
 import { Magnifier } from "../../../util/assets";
+import { searchValue } from "../../../modules/atom/portfolio/search/index";
 import * as S from "./style";
 
 const Searchbar = () => {
   const [isFocusing, setIsFocusing] = useState<boolean>(false);
+  const setSearchContent = useSetRecoilState(searchValue);
   const searchInputRef = useRef<any>(null);
+
+  const searchHandler = (e: React.KeyboardEvent<HTMLInputElement> | any) => {
+    if (e.key === "Enter") {
+      setSearchContent(e.target.value);
+      console.log(e.target.value);
+    }
+  };
 
   const focusOn = useCallback(() => {
     setIsFocusing(true);
@@ -28,6 +38,7 @@ const Searchbar = () => {
           onBlur={focusOff}
           onFocus={focusOn}
           ref={searchInputRef}
+          onKeyUp={(e) => searchHandler(e)}
         />
         <img src={Magnifier} alt="검색아이콘" />
       </S.Input>
