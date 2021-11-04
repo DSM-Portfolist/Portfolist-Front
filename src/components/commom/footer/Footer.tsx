826b9f84136cbcb84e0d646a29a104bd42b26fdf
강은빈 <kub0803@naver.com>
2021-11-04 17:09:02 +0900

    [ADD] Footer 컴포넌트 추가

---
+++ src/components/commom/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const Footer = () => {
+  return(
+      <S.FooterWrap>
+          
+      </S.FooterWrap>
+  );
+};
+
+export default Footer;
---
+++ src/components/commom/footer/style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { mainColor } from "../../../util/css/color/color";
+
+export const FooterWrap = styled.section`
+  background-color: ${mainColor};
+  width: 100%;
+  height: 400px;
+`;
