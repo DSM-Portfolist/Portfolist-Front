14bfa9f2ad00213ef19717a087e27853e04ca60f
JaeHyun <soehd0889@naver.com>
2021-11-07 23:37:30 +0900

    [ADD] image 제작 퍼블리싱

--- src/components/portfolioMake/imageContainerList/ImageContainerList.tsx
+++ src/components/portfolioMake/imageContainerList/ImageContainerList.tsx
@@ -14,7 +14,16 @@ const ImageContainerList = () => {
           placeholder="자신만의 특별한 경험(경력, 인턴, 교육 등)이 있다면 입력해주세요."
         />
         <S.ImageListSection>
-          <S.ImageWrapper></S.ImageWrapper>
+          <S.ImageWrapper>
+            <img src="" alt="" />
+            <input
+              type="file"
+              id="input-file"
+              style={{ display: "none" }}
+              accept="image/jpg,impge/png,image/jpeg,image/gif"
+            />
+            <label htmlFor="input-file">image</label>
+          </S.ImageWrapper>
           <S.ContentWrapper></S.ContentWrapper>
         </S.ImageListSection>
       </S.ImageWrapeerList>
