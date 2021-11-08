b08ce27b9c2e92b9a7d1891fdeff8afb6344f001
강은빈 <kub0803@naver.com>
2021-11-08 23:48:27 +0900

    [ADD] footer 제작

--- src/components/commom/footer/Footer.tsx
+++ src/components/commom/footer/Footer.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { FooterGithub, FooterLogo } from "../../../util/assets";
 import * as S from "./style";
 
 const Footer = () => {
-  return(
-      <S.FooterWrap>
-          
-      </S.FooterWrap>
+  return (
+    <S.FooterWrap>
+      <div className="logo">
+        <img src={FooterLogo} alt="로고" />
+        <span>포트폴리스트는 여러분을 응원합니다.</span>
+        <span>© portfolist</span>
+      </div>
+      <div className="developer-container">
+        <div className="icon">
+          <img src={FooterGithub} alt="깃허브아이콘" />
+        </div>
+        <ul>
+          <li>FRONTEND</li>
+          <li>강은빈</li>
+          <li>김재현</li>
+        </ul>
+        <ul>
+          <li>SERVER</li>
+          <li>고도현</li>
+          <li>곽도현</li>
+        </ul>
+      </div>
+    </S.FooterWrap>
   );
 };
 
--- src/components/commom/footer/style.ts
+++ src/components/commom/footer/style.ts
@@ -1,8 +1,47 @@
 import styled from "@emotion/styled";
-import { mainColor } from "../../../util/css/color/color";
 
 export const FooterWrap = styled.section`
-  background-color: ${mainColor};
+  background-color: #f5927d;
   width: 100%;
   height: 400px;
+  box-sizing: border-box;
+  padding: 3% 13%;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .logo {
+    display: flex;
+    flex-direction: column;
+
+    & img {
+      width: 110px;
+    }
+    & span {
+      margin-top: 10px;
+    }
+  }
+
+  .developer-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    .icon img {
+      width: 30px;
+    }
+
+    & ul {
+      margin: 0 0 0 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      & li {
+        font-weight: 600;
+        margin-top: 5px;
+      }
+    }
+  }
 `;
