a75dc31b0cdf4c700438eee08302b00f72da0e36
JaeHyun <soehd0889@naver.com>
2021-11-08 23:17:10 +0900

    [DELETE] 스타일 이전 삭제

--- src/components/portfolioMake/imageContainerList/style.ts
+++ src/components/portfolioMake/imageContainerList/style.ts
@@ -32,51 +32,12 @@ export const ImageWrapeerList = styled.div`
 `;
 
 export const ImageListSection = styled.section`
-  padding: 30px 30px;
+  padding: 25px 25px;
   background: #ffffff;
   border: 1px solid #eaeaea;
   box-sizing: border-box;
   margin-top: 15px;
   display: flex;
-`;
-
-export const ImageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  width: 42%;
-  label {
-    cursor: pointer;
-    width: 300px;
-    height: 190px;
-    background-color: #f2f2f2;
-    border: dotted #979797 1px;
-    text-align: center;
-    margin: auto 0;
-    line-height: 190px;
-    color: #989898;
-  }
-`;
-
-export const ImageItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-  .PreviewURL {
-    width: 300px;
-    height: 190px;
-    object-fit: cover;
-    border: dotted #979797 1px;
-  }
-  span {
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-    color: #bbbbbb;
-    margin-top: 11px;
-    cursor: pointer;
-    text-align: center;
-  }
 `;
 
 export const ContentWrapper = styled.div`
