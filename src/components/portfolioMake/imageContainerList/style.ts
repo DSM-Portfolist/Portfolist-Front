3a8454cad7caa4806ded3abe53ee1b0571443b8b
JaeHyun <soehd0889@naver.com>
2021-11-08 22:48:38 +0900

    [ADD] 이미지 리스트 스타일 추가

--- src/components/portfolioMake/imageContainerList/style.ts
+++ src/components/portfolioMake/imageContainerList/style.ts
@@ -43,9 +43,11 @@ export const ImageListSection = styled.section`
 export const ImageWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   width: 42%;
   label {
-    width: 280px;
+    cursor: pointer;
+    width: 300px;
     height: 190px;
     background-color: #f2f2f2;
     border: dotted #979797 1px;
@@ -53,6 +55,27 @@ export const ImageWrapper = styled.div`
     margin: auto 0;
     line-height: 190px;
     color: #989898;
+  }
+`;
+
+export const ImageItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  .PreviewURL {
+    width: 300px;
+    height: 190px;
+    object-fit: cover;
+    border: dotted #979797 1px;
+  }
+  span {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    color: #bbbbbb;
+    margin-top: 11px;
+    cursor: pointer;
+    text-align: center;
   }
 `;
 
