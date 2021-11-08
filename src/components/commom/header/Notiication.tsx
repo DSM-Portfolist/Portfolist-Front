70fd7b97b3d7de8fc5ed46d3a679f2c5408ddd1e
강은빈 <kub0803@naver.com>
2021-11-08 22:50:35 +0900

    [FEAT] 알림 API 연동

--- src/components/commom/header/Notiication.tsx
+++ src/components/commom/header/Notiication.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { notiBox } from "../../../modules/atom/header";
+import { notificationSelector } from "../../../modules/atom/main";
 import { NoNotification, NotiIcon } from "../../../util/assets";
+import NotiItem from "./NotiItem";
 import * as S from "./style";
 
 const Notiication = () => {
   const [noti, setNoti] = useRecoilState(notiBox);
+  const notiValue = useRecoilValue(notificationSelector);
   const test = true;
 
   return (
@@ -19,7 +22,11 @@ const Notiication = () => {
       <S.Notification
         noti={noti}
         style={noti ? { height: 200 } : { height: 0 }}
-      ></S.Notification>
+      >
+        {notiValue.map((item, index) => (
+          <NotiItem key={index} name={item.name} type={item.type} />
+        ))}
+      </S.Notification>
     </S.NotiWrapper>
   );
 };
