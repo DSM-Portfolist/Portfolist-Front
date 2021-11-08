0b8741f56f85796568a53057c9e2b1a372608e40
JaeHyun <soehd0889@naver.com>
2021-11-08 23:30:53 +0900

    [ADD] 인터페이스 추가

--- src/util/interface/portfolio/portfolioMakeType.ts
+++ src/util/interface/portfolio/portfolioMakeType.ts
@@ -2,6 +2,12 @@ export interface InputIsNullType {
   isNull: boolean;
 }
 
+export interface inputDataArrType {
+  id: string;
+  name: string;
+  content: string;
+}
+
 export interface imageListType {
   isInFile: boolean;
   index: number;
