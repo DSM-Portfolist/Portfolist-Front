ae3c09f483a5eaf284bc11e2e7ab681ce7a59860
강은빈 <kub0803@naver.com>
2021-11-09 23:53:24 +0900

    [ADD] 마이페이지 포트폴리오 인터페이스 추가

---
+++ src/util/interface/MyPage/myPortfolioType.ts
@@ -0,0 +1,11 @@
+export interface MyPortfolioType {
+  id: number;
+  title: string;
+  comment: number;
+  date: string | undefined;
+  field: string[];
+  icon: string;
+  introduce: string;
+  touching: number;
+  url: string;
+}
