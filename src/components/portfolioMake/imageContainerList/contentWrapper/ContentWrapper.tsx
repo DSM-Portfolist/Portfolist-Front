e36c94e1fc708ff3dcb95eb0fbab16d341449fc7
JaeHyun <soehd0889@naver.com>
2021-11-09 20:23:48 +0900

    [ADD] 리스트 추가 삭제 기능 구현

--- src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
+++ src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { MinusButton } from "../../../../util/assets";
 import * as S from "./style";
+import { ToastSuccess, ToastError } from "../../../../hook/toastHook";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContentWrapper = () => {
+  const [boxData, setBoxData] = useState<any[]>([
+    { box_title: "", box_content: "" },
+  ]);
+
+  console.log(boxData);
+
+  const AddBoxData = () => {
+    setBoxData([...boxData, { box_title: "", box_content: "" }]);
+  };
+
+  const DeleteBoxData = (i:number) => {
+      if (boxData.length === 1) {
+        ToastError("삭제할 수 없습니다");
+      } else {
+        setBoxData(
+          boxData?.filter((arrItem: any, index:number) => {
+            return index !== i;
+          })
+        );
+      }
+    };
+
   return (
     <S.ContentContainer>
-      <div className="Title">
-        <input placeholder="제목을 입력해주세요." />
-        <img src={MinusButton} alt="MinusButton" />
-      </div>
-      <textarea
-        placeholder="내용을 입력해주세요."
-        className="Content"
-      ></textarea>
-      <span
-        className="addContent"
-        onClick={() => {
-          console.log('test');
-        }}
-      >
-        내용 추가
-      </span>
+      <ToastContainer/>
+      {boxData.map((value: any, index: number) => {
+        return (
+          <S.BoxItem>
+            <div className="Title">
+              <input placeholder="제목을 입력해주세요." />
+              <img src={MinusButton} alt="MinusButton" onClick={()=> {DeleteBoxData(index)}} />
+            </div>
+            <textarea
+              placeholder="내용을 입력해주세요."
+              className="Content"
+            ></textarea>
+            {index + 1 < boxData.length  ? "" : <span className="addContent" onClick={AddBoxData}>내용 추가</span>}
+          </S.BoxItem>
+        );
+      })}
     </S.ContentContainer>
   );
 };
