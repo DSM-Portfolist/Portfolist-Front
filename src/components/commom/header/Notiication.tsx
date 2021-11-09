77e2fd3f021691119b42ab8fc81ee08b3a2f621a
강은빈 <kub0803@naver.com>
2021-11-09 20:27:01 +0900

    [MODIFY] notificaton api

--- src/components/commom/header/Notiication.tsx
+++ src/components/commom/header/Notiication.tsx
@@ -2,20 +2,19 @@ import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { notiBox } from "../../../modules/atom/header";
 import { notificationSelector } from "../../../modules/selector/mainpage";
-import { NoNotification, NotiIcon } from "../../../util/assets";
+import { NoNotification, Notification } from "../../../util/assets";
 import NotiItem from "./NotiItem";
 import * as S from "./style";
 
 const Notiication = () => {
   const [noti, setNoti] = useRecoilState(notiBox);
-  // const notification = useRecoilValue(notificationSelector);
-  const test = true;
+  const notification = useRecoilValue(notificationSelector);
 
   return (
     <S.NotiWrapper>
       <img
         className="noti-img"
-        src={test ? `${NoNotification}` : `${NotiIcon}`}
+        src={notification.length > 0 ? `${Notification}` : `${NoNotification}`}
         alt="알림아이콘"
         onClick={() => setNoti(!noti)}
       />
@@ -23,9 +22,9 @@ const Notiication = () => {
         noti={noti}
         style={noti ? { height: 200 } : { height: 0 }}
       >
-        {/*  {notification.map((item, index) => (
+        {notification.map((item, index) => (
           <NotiItem key={index} name={item.name} type={item.type} />
-        ))} */}
+        ))}
       </S.Notification>
     </S.NotiWrapper>
   );
