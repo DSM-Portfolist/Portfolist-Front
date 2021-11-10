f943ee5a1eb492a298ffe97e0455ba7b0561a3bf
강은빈 <kub0803@naver.com>
2021-11-10 15:04:55 +0900

    [ADD] tochingPortfolio atom

--- src/modules/atom/userpage/mypage.ts
+++ src/modules/atom/userpage/mypage.ts
@@ -1,11 +1,31 @@
-import { selector } from "recoil";
-import { getMyPortfolioList } from "../../../util/api/user/portfolio";
-import { MyPortfolioType } from "../../../util/interface/MyPage/myPortfolioType";
+import { atom, selector } from "recoil";
+import {
+  getMyPortfolioList,
+  getMyTouchingPortfolioList,
+} from "../../../util/api/user/portfolio";
+import {
+  MyPortfolioType,
+  MyTouchingPortfolioType,
+} from "../../../util/interface/MyPage/myPortfolioType";
 
+export const myPortfolioList = atom<
+  MyPortfolioType[] | MyTouchingPortfolioType[]
+>({
+  key: "portfolioList",
+  default: [],
+});
 export const myPortfolioListSelector = selector<MyPortfolioType[]>({
   key: "myPortfolioList/get",
   get: async () => {
     const res = await getMyPortfolioList();
     return res.data;
+  },
+});
+
+export const myTouchingPortfolioSelector = selector({
+  key: "myTouchingPortfolio/get",
+  get: async () => {
+    const res = await getMyTouchingPortfolioList();
+    return res.data.content;
   },
 });
