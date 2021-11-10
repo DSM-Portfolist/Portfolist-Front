99b8d3d89a2b091f1568576bf2b8ee696445c911
JaeHyun <soehd0889@naver.com>
2021-11-10 19:02:13 +0900

    [ADD] 박스 내용 추가 form 기능 추가

--- src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
+++ src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MinusButton } from "../../../../util/assets";
 import * as S from "./style";
 import { ToastSuccess, ToastError } from "../../../../hook/toastHook";
@@ -7,42 +7,85 @@ import "react-toastify/dist/ReactToastify.css";
 
 const ContentWrapper = () => {
   const [boxData, setBoxData] = useState<any[]>([
-    { box_title: "", box_content: "" },
+    { id: 0, box_title: "", box_content: "" },
   ]);
 
-  console.log(boxData);
-
+  /* useEffect(() => {
+    console.log(boxData);
+  }, [boxData]);
+ */
   const AddBoxData = () => {
-    setBoxData([...boxData, { box_title: "", box_content: "" }]);
+    var jbRandom = Math.random();
+    setBoxData([
+      ...boxData,
+      { id: boxData.length + jbRandom, box_title: "", box_content: "" },
+    ]);
   };
 
-  const DeleteBoxData = (i:number) => {
-      if (boxData.length === 1) {
-        ToastError("삭제할 수 없습니다");
-      } else {
-        setBoxData(
-          boxData?.filter((arrItem: any, index:number) => {
-            return index !== i;
-          })
-        );
-      }
-    };
+  const DeleteBoxData = (i: number) => {
+    if (boxData.length === 1) {
+      ToastError("삭제할 수 없습니다");
+    } else {
+      setBoxData(
+        boxData?.filter((arrItem: any) => {
+          console.log(arrItem.id, i);
+          return arrItem.id !== i;
+        })
+      );
+    }
+  };
+
+  const handdleOnchange = (e: any, id: number) => {
+    setBoxData(
+      boxData.map((value: any) => {
+        if (value.id === id) {
+          return { ...value, [e.target.name]: e.target.value };
+        } else {
+          return value;
+        }
+      })
+    );
+  };
 
   return (
     <S.ContentContainer>
-      <ToastContainer/>
+      <ToastContainer />
       {boxData.map((value: any, index: number) => {
         return (
           <S.BoxItem>
             <div className="Title">
-              <input placeholder="제목을 입력해주세요." />
-              <img src={MinusButton} alt="MinusButton" onClick={()=> {DeleteBoxData(index)}} />
+              <input
+                placeholder="제목을 입력해주세요."
+                name="box_title"
+                value={value.box_title}
+                onChange={(e: any) => {
+                  handdleOnchange(e, value.id);
+                }}
+              />
+              <img
+                src={MinusButton}
+                alt="MinusButton"
+                onClick={() => {
+                  DeleteBoxData(value.id);
+                }}
+              />
             </div>
             <textarea
               placeholder="내용을 입력해주세요."
               className="Content"
+              name="box_content"
+              onChange={(e: any) => {
+                handdleOnchange(e, value.id);
+              }}
+              value={value.box_content}
             ></textarea>
-            {index + 1 < boxData.length  ? "" : <span className="addContent" onClick={AddBoxData}>내용 추가</span>}
+            {index + 1 < boxData.length ? (
+              ""
+            ) : (
+              <span className="addContent" onClick={AddBoxData}>
+                내용 추가
+              </span>
+            )}
           </S.BoxItem>
         );
       })}
