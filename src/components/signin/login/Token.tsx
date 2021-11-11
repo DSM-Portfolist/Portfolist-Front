09a9976df71302663ce554bba99878ca7f389665
강은빈 <kub0803@naver.com>
2021-11-11 16:13:20 +0900

    [FEAT] 깃허브 로그인 완료

--- src/components/signin/login/Token.tsx
+++ src/components/signin/login/Token.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react";
 import { github } from "../../../util/api/auth/github";
 import queryString from "query-string";
+import { useHistory } from "react-router";
 
 const Token = ({ location }: any) => {
+  const history = useHistory();
   const code = queryString.parse(location.search);
   console.dir(code.code);
 
   useEffect(() => {
-    github(code.code);
-  }, [code.code]);
+    github(code.code)
+      .then((res) => {
+        localStorage.setItem("access_token_portfolist", res.data.access_token);
+        localStorage.setItem(
+          "refresh_token_portfolist",
+          res.data.refresh_token
+        );
+        history.push("/");
+      })
+      .catch((e) => console.log(e));
+  }, [code.code, history]);
 
   return <div>hello</div>;
 };
