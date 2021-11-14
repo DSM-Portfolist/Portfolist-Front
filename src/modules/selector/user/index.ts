44805b62b95da630cf9d003283bdbc413742a706
강은빈 <kub0803@naver.com>
2021-11-14 12:22:30 +0900

    [ADD] userinfo selector

--- src/modules/selector/user/index.ts
+++ src/modules/selector/user/index.ts
@@ -1,6 +1,17 @@
-import { selector } from "recoil";
+import { atom, selector } from "recoil";
 import { getUser } from "../../../util/api/user/info";
 import { UserInfoType } from "../../../util/interface/user";
+
+export const userInfoValue = atom<UserInfoType>({
+  key: "userInfo",
+  default: {
+    field: [""],
+    introduce: "",
+    name: "string",
+    profile_img: "string",
+    github_user: false,
+  },
+});
 
 // 마이페이지 유저 정보
 export const myInfoSelector = selector<UserInfoType>({
@@ -8,5 +19,8 @@ export const myInfoSelector = selector<UserInfoType>({
   get: async () => {
     const res = await getUser();
     return res.data;
+  },
+  set: ({ set }, newValue) => {
+    set(userInfoValue, newValue);
   },
 });
