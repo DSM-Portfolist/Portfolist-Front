7857ea41aa3c3f1b0171934489e95cddf61e30c0
JaeHyun <soehd0889@naver.com>
2021-11-14 10:09:45 +0900

    [ADD] 자격증 컴포넌트 스타일 추가

--- src/components/portfolioMake/licenseContainer/LicenseContainer.tsx
+++ src/components/portfolioMake/licenseContainer/LicenseContainer.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import * as S from "./style";
 
 const LicenseContainer = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <S.LicenseWrapper className="make-container"></S.LicenseWrapper>;
 };
 
 export default LicenseContainer;
--- src/components/portfolioMake/licenseContainer/style.ts
+++ src/components/portfolioMake/licenseContainer/style.ts
@@ -1,1 +1,6 @@
 import styled from "@emotion/styled";
+
+
+export const LicenseWrapper = styled.section`
+  
+`
