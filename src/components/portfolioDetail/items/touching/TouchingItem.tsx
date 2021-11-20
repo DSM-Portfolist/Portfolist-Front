c0d2478c0b010ea974a8d24eac1c7dfa72020deb
강은빈 <kub0803@naver.com>
2021-11-20 23:38:22 +0900

    [FEAT] 포트플리오 상세보기 터칭 추가

---
+++ src/components/portfolioDetail/items/touching/TouchingItem.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useMutation } from "react-query";
+import { useRecoilValue } from "recoil";
+import { CountChangeHook } from "../../../../hook/countChangeHook";
+import { getPortfolioSelecor } from "../../../../modules/atom/portfolio/portfolioDetail";
+import {
+  deleteTouching,
+  postTouching,
+} from "../../../../util/api/portfolio/useTouching";
+import { BeforeTouching, Touching } from "../../../../util/assets";
+import * as S from "./style";
+
+const TouchingItem = () => {
+  const portfolioValue = useRecoilValue(getPortfolioSelecor);
+  const [touchingBoolean, setTouchingBoolean] = useState<boolean>(
+    portfolioValue.touched
+  );
+  const [count, setCount] = useState<number>(portfolioValue.total_touching);
+
+  const touching = useMutation("touching", postTouching);
+  const untouching = useMutation("untouching", deleteTouching);
+
+  return (
+    <S.Touching>
+      <S.TouchingBox>
+        <S.TouchingBack
+          onClick={() => {
+            setTouchingBoolean(!touchingBoolean);
+            setCount(CountChangeHook(touchingBoolean, count));
+            touchingBoolean
+              ? touching.mutate(portfolioValue.portfolio_id)
+              : untouching.mutate(portfolioValue.portfolio_id);
+          }}
+        >
+          <img
+            src={touchingBoolean ? `${Touching}` : `${BeforeTouching}`}
+            alt="터칭하기"
+          />
+        </S.TouchingBack>
+        <span>{count === 0 ? "0" : count}</span>
+      </S.TouchingBox>
+    </S.Touching>
+  );
+};
+
+export default TouchingItem;
