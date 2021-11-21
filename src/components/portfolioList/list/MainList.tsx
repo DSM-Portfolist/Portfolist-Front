e557c8941b86ef494abff75d7bfd4801c25ee18f
강은빈 <kub0803@naver.com>
2021-11-21 20:18:04 +0900

    [FEAT] 검색

--- src/components/portfolioList/list/MainList.tsx
+++ src/components/portfolioList/list/MainList.tsx
@@ -1,37 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./style";
 import { Search, ListItem } from "../..";
 import { PortListType } from "../../../util/interface/portfolio/portListType";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { getPortListSelector } from "../../../modules/atom/portfolio";
-import { searchValue } from "../../../modules/atom/portfolio/search/index";
+import { useLocation } from "react-router";
+import QueryString from "query-string";
+import { getSearch } from "../../../util/api/portfolio/portfolio";
 
 const MainList = () => {
-  const portfolioList = useRecoilValue(getPortListSelector);
-  const searchContent = useRecoilValue(searchValue);
+  const [portfolioList, setPortoflioList] = useRecoilState(getPortListSelector);
+  const location = useLocation();
+  const queryData = QueryString.parse(location.search);
+  const query = queryData.query;
 
-  console.log(searchContent.length);
+  useEffect(() => {
+    const getPorfolio = async () => {
+      try {
+        const search = await getSearch(query, "title");
+        //console.log(search.data);
+        setPortoflioList(search.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getPorfolio();
+    console.log(portfolioList);
+  }, [portfolioList, query, setPortoflioList]);
 
   return (
     <S.MainListWrapper className="container">
       <Search />
       <S.ListWrapper>
-        {portfolioList?.portfolio_list.length === 0 ? (
+        {portfolioList?.length === 0 ? (
           <p className="list-not-comment">작성된 포트폴리오가 없습니다</p>
         ) : (
           <>
-            {searchContent.length === 0 ? (
+            {query?.length === 0 ? (
               ""
             ) : (
-              <S.SearchContent>{searchContent}의 검색결과...</S.SearchContent>
+              <S.SearchContent>{query}의 검색결과...</S.SearchContent>
             )}
 
             <S.ListContent>
-              {portfolioList?.portfolio_list.map(
-                (list: PortListType, index: number) => (
-                  <ListItem key={index} list={list} />
-                )
-              )}
+              {portfolioList?.map((list: PortListType, index: number) => (
+                <ListItem key={index} list={list} />
+              ))}
             </S.ListContent>
           </>
         )}
