767b0d8c59b67b69b4e3f970ff5aa253b316e4ce
강은빈 <kub0803@naver.com>
2021-11-21 23:58:13 +0900

    [ADD] field 추가

--- src/components/main/Main/filterBar/FilterBar.tsx
+++ src/components/main/Main/filterBar/FilterBar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Arrow } from "../../../../util/assets";
+import FilterItem from "./FilterItem";
 import * as S from "./style";
 
 const FilterBar = () => {
   return (
     <S.FilterWrapper>
-      <S.FilterItem>
-        <></>
-      </S.FilterItem>
+      <S.FilterItemWrap>
+        <FilterItem />
+        <FilterItem />
+        <FilterItem />
+      </S.FilterItemWrap>
       <S.MoreItem>
         <p>보러가기</p>
         <img src={Arrow} alt="더보거가기 아이콘" />
