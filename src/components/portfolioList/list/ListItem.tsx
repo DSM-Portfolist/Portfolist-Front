897085948f93d2c61a5f3c21ce9d8680c38d2d65
강은빈 <kub0803@naver.com>
2021-11-21 20:18:16 +0900

    [REFACTOR] 코드 정리

--- src/components/portfolioList/list/ListItem.tsx
+++ src/components/portfolioList/list/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { useMutation } from "react-query";
 import { Link } from "react-router-dom";
 import * as S from "./style";
