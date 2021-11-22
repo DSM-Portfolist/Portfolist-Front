5cd0ba4c5e370547dc59596f73667f0b52b0df45
강은빈 <kub0803@naver.com>
2021-11-22 23:50:26 +0900

    [ADD] 배너 선택, 푸터 추가

--- src/components/portfolioMake/portfolioMake.tsx
+++ src/components/portfolioMake/portfolioMake.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header } from "..";
+import { Footer, Header } from "..";
 import * as S from "./style";
 import TitleContainer from "./titleContainer/TitleContainer";
 import MoreInfoContainer from "./moreInfoContainer/MoreInfoContainer";
@@ -22,7 +22,9 @@ const portfolioMake = () => {
         <ImageContainerList /> {/* 자신의 경험을 넣을 수 있는 이미지 리스트 */}
         <LicenseContainer /> {/* 자격증을 넣을 수 있는 리스트 */}
         <BannerContainer />
+        <S.FinshButton>작성완료</S.FinshButton>
       </S.MainContainer>
+      <Footer />
     </>
   );
 };
