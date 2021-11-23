c9b7f49d270c58dd82988f8546adf8f4dc8d8436
JaeHyun <soehd0889@naver.com>
2021-11-23 17:11:01 +0900

    [ADD] 파일이 있을때 이미지 추가

--- src/util/assets/index.ts
+++ src/util/assets/index.ts
@@ -28,6 +28,6 @@ export { default as MinusButton } from "./icon/minusButton.svg";
 export { default as FooterLogo } from "./icon/FooterLogo.png";
 export { default as FooterGithub } from "./icon/FooterGithub.png";
 export { default as File } from "./icon/file.svg";
+export { default as isInFile } from "./icon/isInFile.svg";
 export const DefaultImage =
   "https://www.ibossedu.co.kr/template/DESIGN_shared/program/theme/01/THUMBNAIL_60_60_icon_rep_box.gif";
-
