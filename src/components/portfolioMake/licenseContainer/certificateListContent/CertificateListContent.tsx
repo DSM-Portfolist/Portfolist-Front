ae53934ff709865687b63e915e373636c7ce5846
JaeHyun <soehd0889@naver.com>
2021-11-24 11:23:30 +0900

    [ADD] 내용을 추가하는 컴포넌트

---
+++ src/components/portfolioMake/licenseContainer/certificateListContent/CertificateListContent.tsx
@@ -0,0 +1,69 @@
+import { anyTypeAnnotation } from "@babel/types";
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { certificateListAtom } from "../../../../modules/atom/portfolioPost/certificate";
+import { MinusButton } from "../../../../util/assets";
+import { CertificateListType } from "../../../../util/interface/portfolioPost/postType";
+import * as S from "./style";
+
+interface Props {
+  certificate_content: string[];
+  setCertificateList: any;
+  certificateList: CertificateListType[];
+  textList: any;
+  setTextList: (text: Array<string>) => void;
+}
+
+const CertificateListContent = (props: Props) => {
+  const {
+    certificateList,
+    setCertificateList,
+    certificate_content,
+    textList,
+    setTextList,
+  } = props;
+
+  useEffect(() => {
+    console.log(props.textList);
+  }, [certificate_content, props.textList]);
+
+  const addContentList = () => {
+    setTextList(textList.concat(""));
+  };
+
+  const onChangeEvent = (e: any, index: number) => {
+    const { value } = e.target;
+    textList[index] = value;
+    setTextList(textList);
+  };
+
+  return (
+    <>
+      {certificate_content?.map((item: any, index: number) => {
+        return (
+          <div className="infoContainer">
+            <S.InputBox>
+              <input
+                id="inputContent"
+                onChange={(e) => {
+                  onChangeEvent(e, index);
+                }}
+                placeholder="내용을 입력해주세요."
+              />
+              <img src={MinusButton} alt="" />
+            </S.InputBox>
+            {index + 1 < certificate_content.length ? (
+              ""
+            ) : (
+              <span className="addContent" onClick={addContentList}>
+                내용 추가
+              </span>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default CertificateListContent;
