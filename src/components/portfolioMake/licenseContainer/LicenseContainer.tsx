644edd566aafc684eb3cf3bb0427d04078e687f7
JaeHyun <soehd0889@naver.com>
2021-11-24 11:24:08 +0900

    [ADD] 내용 컴포넌트와 연결하는 코드 추가

--- src/components/portfolioMake/licenseContainer/LicenseContainer.tsx
+++ src/components/portfolioMake/licenseContainer/LicenseContainer.tsx
@@ -1,54 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./style";
-import { PlusButton, MinusButton } from "../../../util/assets";
 import { ToastError } from "../../../hook/toastHook";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { CertificateListType } from "../../../util/interface/portfolioPost/postType";
 import deleteButtonX from "../../../util/assets/icon/deleteButtonX.svg";
+import CertificateListContent from "./certificateListContent/CertificateListContent";
 
 const LicenseContainer = () => {
+  const [textList, setTextList] = useState<string[]>([""]);
   const [certificateList, setCertificateList] = useState<CertificateListType[]>(
-    [{ title: "", certificate_list: [""] }]
+    [{ title: "", certificate_list: textList }]
   );
 
-  console.log(certificateList);
+  useEffect(() => {
+    console.log(certificateList);
+  }, [certificateList]);
+
+  useEffect(() => {
+    updateCertificateList();
+  }, [textList]);
+
+  const updateCertificateList = () => {
+    setCertificateList(
+      certificateList.map((list: any) => {
+        return {
+          ...list,
+          certificate_list: textList,
+        };
+      })
+    );
+  };
 
   const addList = () => {
     setCertificateList((certificateList: CertificateListType[]) => [
       ...certificateList,
       {
         title: "",
-        certificate_list: [""],
+        certificate_list: textList,
       },
     ]);
   };
 
-  const deleteList = (id: number) => {
-    
-  }
-
-  const addContentList = (p_index: number) => {
-    setCertificateList(
-      certificateList.map(
-        (certificateList: CertificateListType, index: number) => {
-          console.log(certificateList);
-          if (p_index === index) {
-            return {
-              ...certificateList,
-              certificate_list: certificateList.certificate_list.concat(""),
-            };
-          } else {
-            return certificateList;
-          }
-        }
-      )
-    );
+  const deleteList = (index: number) => {
+    if (certificateList.length <= 1) {
+      ToastError("삭제할 수 없습니다.");
+    } else {
+      console.log(index);
+    }
   };
-
-  const deleteContentList = () => {
-    
-  }
 
   return (
     <S.LicenseWrapper className="make-container">
@@ -58,51 +58,30 @@ const LicenseContainer = () => {
           + Add new list
         </span>
       </S.HeaderButton>
-      {certificateList.map(
-        (certificateList: CertificateListType, index: number) => {
-          const { certificate_list, title } = certificateList;
-          console.log(certificateList);
-          console.log(index);
-          return (
-            <S.MapWrapper>
-              <S.TitleWrapper>
-                <input type="text" placeholder="제목을 입력해 주세요." />
-                <img
-                  src={deleteButtonX}
-                  style={{ width: "27px", height: "27px", marginLeft: "5px" }}
-                  alt=""
-                />
-              </S.TitleWrapper>
-              {certificate_list.map((certificate_list: any) => {
-                console.log(certificate_list);
-                return (
-                  <div className="infoContainer">
-                    <S.InputBox>
-                      <textarea
-                        id="inputContent"
-                        placeholder="내용을 입력해주세요."
-                      ></textarea>
-                      <img src={MinusButton} alt="" />
-                    </S.InputBox>
-                    {index + 1 < certificate_list.length ? (
-                      ""
-                    ) : (
-                      <span
-                        className="addContent"
-                        onClick={() => {
-                          addContentList(index);
-                        }}
-                      >
-                        내용 추가
-                      </span>
-                    )}
-                  </div>
-                );
-              })}
-            </S.MapWrapper>
-          );
-        }
-      )}
+      {certificateList?.map((list: CertificateListType, index: number) => {
+        return (
+          <S.MapWrapper>
+            <S.TitleWrapper>
+              <input type="text" placeholder="제목을 입력해 주세요." />
+              <img
+                src={deleteButtonX}
+                style={{ width: "27px", height: "27px", marginLeft: "5px" }}
+                onClick={() => {
+                  deleteList(index);
+                }}
+                alt=""
+              />
+            </S.TitleWrapper>
+            <CertificateListContent
+              certificate_content={list?.certificate_list}
+              certificateList={certificateList}
+              setCertificateList={setCertificateList}
+              textList={textList}
+              setTextList={setTextList}
+            />
+          </S.MapWrapper>
+        );
+      })}
     </S.LicenseWrapper>
   );
 };
