04318ab7596dca8e60eee63992e4f5c717750176
강은빈 <kub0803@naver.com>
2021-11-25 00:48:37 +0900

    [FEAT] 마이페이지 github 처리

--- src/components/mypage/mypage/mypageModify/MypageModify.tsx
+++ src/components/mypage/mypage/mypageModify/MypageModify.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { Header } from "../../..";
+import { myInfoSelector } from "../../../../modules/selector/user";
 import * as S from "../../../../util/css/mypage/mypage/mypageModify/style";
 import ImageUploadWrapper from "./ImageUploadWrapper";
 import MyInfoModify from "./MyInfoModify";
@@ -8,6 +10,8 @@ import SecessionModal from "./SecessionModal";
 
 const MypageModify = () => {
   const [modal, setModal] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useRecoilState(myInfoSelector);
+
 
   return (
     <S.MypageModifyContainer modal={modal}>
@@ -15,7 +19,7 @@ const MypageModify = () => {
       <Header />
       <S.MainSection>
         <S.MyProfileWrapper>
-          <ImageUploadWrapper />
+          <ImageUploadWrapper userInfo={userInfo} />
           <MyInfoModify />
         </S.MyProfileWrapper>
         <MypageModifyMainContent setModal={setModal} modal={modal} />
