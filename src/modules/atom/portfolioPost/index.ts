20521ee7fb97fe1b6094e6171cf155f04003635f
JaeHyun <soehd0889@naver.com>
2021-11-27 00:18:29 +0900

    [MODIFY] recoil 수정

--- src/modules/atom/portfolioPost/index.ts
+++ src/modules/atom/portfolioPost/index.ts
@@ -1,31 +1,19 @@
 import { atom } from "recoil";
 import { ContainerListType } from "../../../util/interface/portfolioPost/postType";
 
-const RandomFunc = (num: number) => {
-  var jbRandom = Math.random();
-  return num + jbRandom;
-};
-
 interface BoxDataType {
   id?: number;
   box_title: string;
   box_content: string;
 }
 
-export const box_data = atom<BoxDataType[]>({
-  key: "box_data",
-  default: [{ id: RandomFunc(0), box_title: "", box_content: "" }],
-});
-
-export const container_text = atom<ContainerListType[]>({
+export const container_list = atom<ContainerListType[]>({
   key: "container_text",
   default: [
     {
-      id: RandomFunc(0),
       container_title: "",
       container_text_list: [
         {
-          id: RandomFunc(0),
           box_title: "",
           box_content: "",
         },
