a307c8ebbc7508acc338b8f917634e250910c5f8
JaeHyun <soehd0889@naver.com>
2021-11-27 00:20:19 +0900

    [ADD] 함수 기능 추가

--- src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
+++ src/components/portfolioMake/imageContainerList/contentWrapper/ContentWrapper.tsx
@@ -5,46 +5,68 @@ import { ToastSuccess, ToastError } from "../../../../hook/toastHook";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRecoilState } from "recoil";
-import {
-  box_data,
-  container_text,
-} from "../../../../modules/atom/portfolioPost";
-import { ContainerTextListType } from "../../../../util/interface/portfolioPost/postType";
+import { container_list } from "../../../../modules/atom/portfolioPost";
 
 const ContentWrapper = (props: any) => {
-  const { setContainerList, identity, updateBoxData } = props;
+  const { parent_index } = props;
+  const [containerList, setContainerList] = useRecoilState(container_list);
 
-  const [boxData, setboxData] = useRecoilState(box_data);
-
-  const addContainerText = () => {
-    var jbRandom = Math.random();
-    setboxData([
-      ...boxData,
-      {
-        id: boxData.length + jbRandom,
-        box_title: "",
-        box_content: "",
-      },
-    ]);
+  const onChangeContainerTextList = (
+    e: any,
+    parent_index: number,
+    index: number
+  ) => {
+    const { name, value } = e.target;
+    setContainerList(
+      containerList.map((item: any, i: number) => {
+        if (parent_index === i) {
+          if (name === "box_title") {
+            containerList[parent_index].container_text_list[index].box_title =
+              value;
+          } else {
+            containerList[parent_index].container_text_list[index].box_content =
+              value;
+          }
+          return {
+            ...containerList[parent_index],
+            container_text_list:
+              containerList[parent_index].container_text_list,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
   };
 
-  const DeleteContainerText = (id: number) => {
-    if (boxData.length === 1) {
+  const DeleteContainerText = (parent_index: number, index: number) => {
+    if (containerList[parent_index].container_text_list.length <= 1) {
       ToastError("삭제할 수 없습니다");
     } else {
-      setboxData(
-        boxData?.filter((arrItem: any, index: number) => {
-          return index !== id;
-        })
+      let newList = containerList[parent_index].container_text_list?.filter(
+        (value: any, i: number) => {
+          return i !== index;
+        }
       );
+      setContainerList((containerList: any) => [
+        { ...containerList[parent_index], container_text_list: newList },
+      ]);
     }
   };
 
-  const handdleOnchange = (e: any, id: number, arr_index: number) => {
-    setboxData(
-      boxData.map((value: any, index: number) => {
-        if (value.id === id) {
-          return { ...value, [e.target.name]: e.target.value };
+  const addContainerText = (parent_index: number) => {
+    setContainerList(
+      containerList.map((value: any, i: number) => {
+        if (parent_index === i) {
+          containerList[parent_index].container_text_list.push({
+            box_title: "",
+            box_content: "",
+          });
+          return {
+            ...containerList[parent_index],
+            container_text_list:
+              containerList[parent_index].container_text_list,
+          };
         } else {
           return value;
         }
@@ -55,46 +77,54 @@ const ContentWrapper = (props: any) => {
   return (
     <S.ContentContainer>
       <ToastContainer />
-      {boxData?.map((value: any, index: number) => {
-        const { box_title, id, box_content } = value;
-        return (
-          <S.BoxItem key={index}>
-            <div className="Title">
-              <input
-                placeholder="제목을 입력해주세요."
-                name="box_title"
-                value={box_title}
+      {containerList[parent_index].container_text_list?.map(
+        (value: any, index: number) => {
+          const { box_content, box_title } = value;
+          return (
+            <S.BoxItem key={index}>
+              <div className="Title">
+                <input
+                  placeholder="제목을 입력해주세요."
+                  name="box_title"
+                  value={box_title}
+                  onChange={(e: any) => {
+                    onChangeContainerTextList(e, parent_index, index);
+                  }}
+                />
+                <img
+                  src={MinusButton}
+                  alt="MinusButton"
+                  onClick={() => {
+                    DeleteContainerText(parent_index, index);
+                  }}
+                />
+              </div>
+              <textarea
+                placeholder="내용을 입력해주세요."
+                className="Content"
+                name="box_content"
+                value={box_content}
                 onChange={(e: any) => {
-                  handdleOnchange(e, id, index);
+                  onChangeContainerTextList(e, parent_index, index);
                 }}
-              />
-              <img
-                src={MinusButton}
-                alt="MinusButton"
-                onClick={() => {
-                  DeleteContainerText(index);
-                }}
-              />
-            </div>
-            <textarea
-              placeholder="내용을 입력해주세요."
-              className="Content"
-              name="box_content"
-              value={box_content}
-              onChange={(e: any) => {
-                handdleOnchange(e, id, index);
-              }}
-            ></textarea>
-            {index + 1 < boxData.length ? (
-              ""
-            ) : (
-              <span className="addContent" onClick={addContainerText}>
-                내용 추가
-              </span>
-            )}
-          </S.BoxItem>
-        );
-      })}
+              ></textarea>
+              {index + 1 <
+              containerList[parent_index].container_text_list.length ? (
+                ""
+              ) : (
+                <span
+                  className="addContent"
+                  onClick={() => {
+                    addContainerText(parent_index);
+                  }}
+                >
+                  내용 추가
+                </span>
+              )}
+            </S.BoxItem>
+          );
+        }
+      )}
     </S.ContentContainer>
   );
 };
