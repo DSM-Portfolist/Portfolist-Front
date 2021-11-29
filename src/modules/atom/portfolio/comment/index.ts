9bc6d05aeaf170a3b96dd46b7cb0e485e29c4f8a
강은빈 <kub0803@naver.com>
2021-11-30 01:40:44 +0900

    [MODIFY] 대댓글 atom

--- src/modules/atom/portfolio/comment/index.ts
+++ src/modules/atom/portfolio/comment/index.ts
@@ -2,6 +2,11 @@ import { atom, atomFamily, selectorFamily } from "recoil";
 import { getComment } from "../../../../util/api/portfolio/comment";
 import { CommentType } from "../../../../util/interface/portfolio/commentType";
 
+export const reCommentControl = atom({
+  key: "reCommentControl",
+  default: false,
+});
+
 export const commentList = atomFamily<CommentType[], number>({
   key: "commentList",
   default: [],
@@ -12,7 +17,7 @@ export const commentContent = atom<string>({
   default: "",
 });
 
-export const commentListSelector = selectorFamily<CommentType[], number>({
+export const commentListSelector = selectorFamily<CommentType[], any>({
   key: "comments/get",
   get: (id) => async () => {
     try {
