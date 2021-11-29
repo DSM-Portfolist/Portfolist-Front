49443d1360337132a31700953157ab023e24b740
JaeHyun <soehd0889@naver.com>
2021-11-30 01:45:46 +0900

    [ADD] 주의사항 텍스트 컴포넌트 추가

---
+++ src/components/portfolioMake/precautionsContainer/style.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const TextContainer = styled.div`
+  & h1 {
+    font-size: 24px;
+    color: #000000;
+    margin-bottom: 14px;
+  }
+  & p {
+    font-size: 22px;
+    color: #7c7c7c;
+    margin-bottom: 6px;
+  }
+  & span {
+    font-size: 18px;
+    color: #c1c1c1;
+    margin-top: 8px;
+  }
+`;
