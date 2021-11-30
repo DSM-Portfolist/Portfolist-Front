0ce42256fe2176eac5e0390bb2488acda1f75466
JaeHyun <soehd0889@naver.com>
2021-11-30 23:25:02 +0900

    [ADD] 포트폴리오 제작 api 호출 코드 작성

--- src/components/portfolioMake/portfolioMake.tsx
+++ src/components/portfolioMake/portfolioMake.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Footer, Header } from "..";
 import * as S from "./style";
 import TitleContainer from "./titleContainer/TitleContainer";
@@ -8,24 +8,37 @@ import LicenseContainer from "./licenseContainer/LicenseContainer";
 import BannerContainer from "./bannerContainer/BannerContainer";
 import FileLinkContainer from "./fileLinkContainer/FileLinkContainer";
 import PrecautionsContainer from "./precautionsContainer/PrecautionsContainer";
+import { useRecoilValue } from "recoil";
+import { portfolioMakeList } from "../../modules/atom/portfolioPost";
+import { portfolioMakeSubmit } from "../../util/api/portfolio/portfolioPost";
 
-const portfolioMake = () => {
+const PortfolioMake = () => {
+  const test = useRecoilValue(portfolioMakeList);
+
+  useEffect(() => {
+    console.log(test);
+  }, [test]);
+
+  const portfolioSubmit = () => {
+    portfolioMakeSubmit(test);
+  };
+
   return (
     <>
       <Header />
       <S.MainContainer>
         <PrecautionsContainer /> {/* 주의사항을 적는 Text 컴포넌트 */}
         <TitleContainer /> {/* 제목 컴포넌트 */}
-        <MoreInfoContainer />{/*이메일이나 깃허브 넣는 컴포넌트*/}
+        <MoreInfoContainer /> {/*이메일이나 깃허브 넣는 컴포넌트*/}
         <ImageContainerList /> {/* 자신의 경험을 넣을 수 있는 이미지 리스트 */}
         <LicenseContainer /> {/* 자격증을 넣을 수 있는 리스트 */}
         <FileLinkContainer /> {/*파일이나 링크를 넣을 수 있는 컴포넌트 */}
         <BannerContainer /> {/* 이미지 배너 선택하는 컴포넌트 */}
-        <S.FinshButton>작성완료</S.FinshButton>
+        <S.FinshButton onClick={portfolioSubmit}>작성완료</S.FinshButton>
       </S.MainContainer>
       <Footer />
     </>
   );
 };
 
-export default portfolioMake;
+export default PortfolioMake;
