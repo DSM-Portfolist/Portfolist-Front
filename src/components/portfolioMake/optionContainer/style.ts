add945a7b9877549e66ab31cab78f6bdf373057f
JaeHyun <soehd0889@naver.com>
2021-12-02 00:00:59 +0900

    [MODIFY] 분야 선택 컴포넌트화

--- src/components/portfolioMake/optionContainer/style.ts
+++ src/components/portfolioMake/optionContainer/style.ts
@@ -26,3 +26,23 @@ export const FieldItemWrapper = styled.div`
     cursor: pointer;
   }
 `;
+
+export const FieldItemContainer = styled.div`
+  margin: 0 10px;
+  width: fit-content;
+  background: #ff7659;
+  border-radius: 20px;
+  padding: 8px 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 14px;
+    color: white;
+  }
+  img {
+    margin: 0 0 0 10px;
+    cursor: pointer;
+  }
+`;
