e7599638da40e0131ffa453c52e5636116771cc4
JaeHyun <soehd0889@naver.com>
2021-12-01 23:35:54 +0900

    [ADD] 분야 select 추가, 퍼블리싱

--- src/components/portfolioMake/optionContainer/OptionContainer.tsx
+++ src/components/portfolioMake/optionContainer/OptionContainer.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { getFieldSelector } from "../../../modules/atom/portfolio";
+import { CloseIcon } from "../../../util/assets";
 import * as S from "./style";
 
 const OptionContainer = () => {
-  return <div></div>;
+  const fieldList = useRecoilValue(getFieldSelector);
+
+  console.log(fieldList);
+
+  const handleSelect = (e: any) => {
+    console.log(e);
+  };
+  
+  return (
+    <S.OptionContainer>
+      <S.FieldItemWrapper>
+        <select
+          onChange={(e) => {
+            console.log(e);
+          }}
+        >
+          <option selected disabled hidden>
+            분야를 선택하세요
+          </option>
+          {fieldList.map((item: any, index: number) => {
+            console.log(item);
+            return (
+              <option key={index} value={item.content}>
+                {item.content}
+              </option>
+            );
+          })}
+        </select>
+      </S.FieldItemWrapper>
+    </S.OptionContainer>
+  );
 };
 
 export default OptionContainer;
