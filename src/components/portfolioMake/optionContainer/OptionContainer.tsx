934d8220144402d50e2afb8d35ec9bfed784ead1
JaeHyun <soehd0889@naver.com>
2021-12-02 20:03:56 +0900

    [ADD] 공개 비공개 컴포넌트 추가

--- src/components/portfolioMake/optionContainer/OptionContainer.tsx
+++ src/components/portfolioMake/optionContainer/OptionContainer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FieldItemWrapper from "./fieldItemWrapper/FieldItemWrapper";
+import IsOpenContainer from "./isOpenContainer/IsOpenContainer";
 import * as S from "./style";
 
 const OptionContainer = () => {
   return (
     <S.OptionContainer>
       <FieldItemWrapper />
+      <IsOpenContainer />
     </S.OptionContainer>
   );
 };
