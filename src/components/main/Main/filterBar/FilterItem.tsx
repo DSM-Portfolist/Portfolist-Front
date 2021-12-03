241e4142ad99fdce5bb3862b058e1122cae4b7c4
강은빈 <kub0803@naver.com>
2021-12-03 18:47:09 +0900

    [MODIFY]

--- src/components/main/Main/filterBar/FilterItem.tsx
+++ src/components/main/Main/filterBar/FilterItem.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { ToastSuccess } from "../../../../hook/toastHook";
-import { getFieldSelector } from "../../../../modules/atom/portfolio";
 import {
   selectFieldList,
   selectFieldNum,
 } from "../../../../modules/atom/portfolio/main";
 import { useFieldValue } from "../../../../modules/atom/portfolio/search";
+import { getField } from "../../../../util/api/portfolio/portfolio";
 import { NoSelectedArrow, SelectedArrow } from "../../../../util/assets";
 import { mainColor } from "../../../../util/css/color/color";
 import { FieldType } from "../../../../util/interface/Sign/loginType";
@@ -15,34 +14,38 @@ import * as S from "./style";
 const FilterItem = () => {
   const [text, setText] = useState<string>("");
   const [arrowSelect, setArrowSelect] = useState<boolean>(false);
-  const field = useRecoilValue(getFieldSelector);
   const [useField, setUseField] = useRecoilState(useFieldValue);
   const [selectField, setSelectField] = useRecoilState(selectFieldList);
   const fieldNum = useRecoilValue(selectFieldNum);
+  const [field, setField] = useState<FieldType[]>();
+
+  useLayoutEffect(() => {
+    const getFieldHandler = async () => {
+      try {
+        const data = await getField();
+        setField(data.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getFieldHandler();
+  }, []);
 
   function UseFieldAdd(field: any) {
-    try {
-      if (selectField.length <= 2) {
-        setUseField(useField?.concat(field));
-        setSelectField(selectField?.concat({ id: fieldNum + 1 }));
-        ToastSuccess("분야는 3개까지 선택 할 수 있습니다.");
-      }
-    } catch (e) {
-      console.log(e);
+    if (selectField.length <= 2) {
+      setUseField(useField?.concat(field));
+      setSelectField(selectField?.concat({ id: fieldNum + 1 }));
     }
   }
 
   useEffect(() => {
-    try {
-      if (selectField.length !== 1) {
-        if (text === "ALL") {
-          setSelectField(
-            selectField?.filter((id: any) => id.id === selectFieldNum)
-          );
-        }
+    if (selectField.length !== 1) {
+      if (text === "ALL") {
+        setSelectField(
+          selectField?.filter((id: any) => id.id === selectFieldNum)
+        );
       }
-    } catch (e) {
-      console.log(e);
     }
   }, [selectField, setSelectField, text, useField]);
 
@@ -78,7 +81,7 @@ const FilterItem = () => {
             >
               ALL
             </li>
-            {field.map((field: FieldType) => (
+            {field?.map((field: FieldType) => (
               <li
                 key={field.id}
                 onClick={() => {
