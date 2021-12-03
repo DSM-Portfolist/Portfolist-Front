9e27857c7a0281e9aae235b5347ff2768b6e2e7b
강은빈 <kub0803@naver.com>
2021-12-03 12:04:08 +0900

    [ADD] reload atom 추가

--- src/modules/atom/auth/index.ts
+++ src/modules/atom/auth/index.ts
@@ -4,3 +4,8 @@ export const tokenAtom = atom<any>({
   key: "tokenAtom",
   default: localStorage.getItem("access_token_portfolist"),
 });
+
+export const reload = atom({
+  key: "reload",
+  default: 0,
+});
