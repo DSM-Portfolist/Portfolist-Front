1b459f821057665f78d75d9a9cf58e885d65e05a
강은빈 <kub0803@naver.com>
2021-12-03 23:24:58 +0900

    [ADD] date 가공 hook 추가

--- src/hook/dateSplitHook.ts
+++ src/hook/dateSplitHook.ts
@@ -1,7 +1,7 @@
-export function DateSplitHook(string: any) {
-  const dateArray = string?.split("-");
-  console.log(dateArray);
-  let date = `${dateArray[0]}년 ${dateArray[1]}월 ${dateArray[2]}일`;
+export function DateSplitHook(string: string | undefined) {
+  let dateArray = string?.split("T");
+  let dateList = dateArray?.[0].split("-");
+  let date = `${dateList?.[0]}년 ${dateList?.[1]}월 ${dateList?.[2]}일`;
 
-  return;
+  return date;
 }
