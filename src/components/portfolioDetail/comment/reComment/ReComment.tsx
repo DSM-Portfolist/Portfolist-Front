5d4239b21e777fcba2b29f61d90a6423a41ca837
강은빈 <kub0803@naver.com>
2021-12-04 23:54:54 +0900

    [FEAT] 대댓글 기능 추가

--- src/components/portfolioDetail/comment/reComment/ReComment.tsx
+++ src/components/portfolioDetail/comment/reComment/ReComment.tsx
@@ -1,75 +1,104 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { ToastSuccess } from "../../../../hook/toastHook";
-import { reCommentControl } from "../../../../modules/atom/portfolio/comment";
-import { deleteReComment } from "../../../../util/api/portfolio/comment";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { ToastError, ToastSuccess } from "../../../../hook/toastHook";
+import {
+  deleteReComment,
+  getReComment,
+  postReComment,
+} from "../../../../util/api/portfolio/comment";
 import { DefaultProfile } from "../../../../util/assets";
-import { CommentType } from "../../../../util/interface/portfolio/commentType";
-import * as S from "../style";
+import {
+  CommentType,
+  ReCommentType,
+} from "../../../../util/interface/portfolio/commentType";
+import * as S from "./style";
 
 interface Props {
   comment: CommentType;
-  getTest: () => void;
 }
 
-const ReComment = ({ comment, getTest }: Props) => {
-  const [reComment, setReComment] = useRecoilState(reCommentControl);
+const ReComment = ({ comment }: Props) => {
+  const [reCommentText, setReCommentText] = useState<string>("");
+  const [reCommentList, setReCommentList] = useState<ReCommentType[]>([]);
 
-  const CommentDelete = (id: number) => {
+  const reComment = useCallback(() => {
+    getReComment(comment.comment_id).then((res) => {
+      setReCommentList(res.data);
+    });
+  }, []);
+
+  const reCommentDeleteHandler = (id: number) => {
     deleteReComment(id)
       .then(() => {
-        ToastSuccess("댓글이 삭제되었습니다.");
-        getTest();
+        ToastSuccess("답글이 삭제되었습니다.");
+        reComment();
       })
       .catch((e) => {
+        ToastError("답글 삭제가 실패했습니다.");
         console.log(e);
       });
   };
 
-  return (
-    <>
-      {comment?.re_comment_list?.map((rc) => (
-        <>
-          <S.ReComment key={rc.re_comment_id}>
-            <S.Content>
-              <img
-                src={
-                  rc?.user?.profile_img === null
-                    ? `${DefaultProfile}`
-                    : rc.user.profile_img
-                }
-                alt="프로필 사진"
-              />
-              <div className="content">
-                <div className="user-name">
-                  <strong>{rc.user.name}</strong>
-                  <div className="comment-date">
-                    <span>{rc.rc_date}</span>
-                  </div>
-                </div>
-                <pre>{rc.re_comment_content}</pre>
-              </div>
-            </S.Content>
-            <S.Util>
-              {rc?.mine ? (
-                <span onClick={() => CommentDelete(rc.re_comment_id)}>
-                  삭제
-                </span>
-              ) : (
-                ""
-              )}
+  const reCommentAddHandler = (e: any) => {
+    const { value } = e.target;
+    setReCommentText(value);
 
-              <span>신고</span>
-            </S.Util>
-          </S.ReComment>
-          <S.Input
-            reComment={reComment}
-            type="text"
-            placeholder="답글을 작성해 주세요"
-          />
-        </>
+    if (e.key === "Enter") {
+      postReComment(comment.comment_id, reCommentText)
+        .then(() => {
+          ToastSuccess("답글이 작성되었습니다.");
+          reComment();
+        })
+        .catch(() => ToastError("답글 작성에 실패했습니다."));
+    }
+  };
+
+  useLayoutEffect(() => {
+    reComment();
+    console.log(reCommentList);
+  }, []);
+
+  return (
+    <S.ReCommentWrap>
+      <S.CommentInputWrap>
+        <S.Input
+          type="text"
+          placeholder="답글을 작성해 주세요"
+          onKeyPress={(e) => reCommentAddHandler(e)}
+        />
+      </S.CommentInputWrap>
+      {reCommentList?.map((rc) => (
+        <S.ReComment key={rc.re_comment_id}>
+          <S.Content>
+            <img
+              src={
+                rc?.user?.profile_img === null
+                  ? `${DefaultProfile}`
+                  : rc.user.profile_img
+              }
+              alt="프로필 사진"
+            />
+            <div className="content">
+              <div className="user-name">
+                <strong>{rc.user.name}</strong>
+                <div className="comment-date">
+                  <span>{rc.rc_date}</span>
+                </div>
+              </div>
+              <pre>{rc.re_comment_content}</pre>
+            </div>
+          </S.Content>
+          <S.Util>
+            {rc?.mine && (
+              <span onClick={() => reCommentDeleteHandler(rc.re_comment_id)}>
+                삭제
+              </span>
+            )}
+
+            <span>신고</span>
+          </S.Util>
+        </S.ReComment>
       ))}
-    </>
+    </S.ReCommentWrap>
   );
 };
 
---
+++ src/components/portfolioDetail/comment/reComment/style.ts
@@ -0,0 +1,100 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  toggle?: boolean;
+}
+
+export const ReCommentWrap = styled.div<Props>`
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  border: 1px solid red;
+`;
+
+export const CommentInputWrap = styled.div`
+  width: 100%;
+  height: 100px;
+  border: 1px solid red;
+`;
+
+export const Input = styled.input<Props>`
+  width: 100%;
+  display: flex;
+  background-color: #efefef;
+  border-radius: 20px;
+  padding: 8px 15px;
+  box-sizing: border-box;
+`;
+
+export const Content = styled.div<Props>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 0;
+
+  a {
+    color: black;
+  }
+
+  img {
+    margin: 0 10px;
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 50px;
+  }
+
+  .content {
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+
+    .user-name {
+      display: flex;
+      flex-direction: row;
+
+      .comment-date {
+        margin-left: 5px;
+        padding-left: 5px;
+        border-left: 1px solid #f0f0f0;
+        color: #757575;
+      }
+    }
+
+    pre {
+      margin-top: 5px;
+    }
+  }
+
+  .more_text {
+    margin-top: 10px;
+    padding: 0 5px;
+    width: fit-content;
+    cursor: pointer;
+  }
+`;
+
+export const Util = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  span {
+    padding: 0 5px;
+    color: #757575;
+    font-size: 14px;
+    cursor: pointer;
+  }
+`;
+
+export const ReComment = styled.div<Props>`
+  width: 90%;
+  margin-left: 10%;
+  border-top: 1px solid #f0f0f0;
+
+  display: ${({ toggle }) => (toggle ? "flex" : "none")};
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
