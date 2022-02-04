7741fa06013f7065aba3ff356038aa439f0a01cf
JaeHyun <soehd0889@naver.com>
2022-02-04 21:39:36 +0900

    .

--- src/components/index.ts
+++ src/components/index.ts
@@ -17,7 +17,7 @@ export { default as FieldItem } from "./portfolioList/search/FieldItem";
 export { default as Search } from "./portfolioList/search/Search";
 export { default as PortfolioDatail } from "./portfolioDetail/PortfolioDetail";
 export { default as Comment } from "./portfolioDetail/comment/Comment";
-export { default as Title } from "./portfolioDetail/items/title/Title";
+export { default as Title } from "./portfolioDetail/items/Title/Title";
 export { default as ExperienceList } from "./portfolioDetail/items/experience/ExperienceList";
 export { default as PortfolioMake } from "./portfolioMake/PortfolioMake";
 export { default as Footer } from "./commom/footer/Footer";
