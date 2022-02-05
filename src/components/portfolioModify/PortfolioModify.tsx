a7ec2b9e61033b6af01def124adb66162562958a
JaeHyun <soehd0889@naver.com>
2022-02-06 01:34:40 +0900

    [MODIFY] button name change

--- src/components/portfolioModify/PortfolioModify.tsx
+++ src/components/portfolioModify/PortfolioModify.tsx
@@ -107,7 +107,7 @@ const PortfolioModify = () => {
         <LicenseContainer /> {/* 자격증을 넣을 수 있는 리스트 */} {/* 수정 완료 */}
         <FileLinkContainer /> {/*파일이나 링크를 넣을 수 있는 컴포넌트 */} {/* 수정 완료 */}
         <BannerContainer /> {/* 이미지 배너 선택하는 컴포넌트 */} {/* 수정 완료 */} 
-        <S.FinshButton onClick={portfolioSubmit}>작성완료</S.FinshButton>
+        <S.FinshButton onClick={portfolioSubmit}>수정완료</S.FinshButton>
       </S.MainContainer>
       <Footer />
     </>
