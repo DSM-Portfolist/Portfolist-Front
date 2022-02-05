8a9b5bff3a3274f84269ab8dc051bd7c2ed5d116
JaeHyun <soehd0889@naver.com>
2022-02-05 16:42:06 +0900

    [ADD] modify imageContainer atom 추가

--- src/modules/atom/portfolioModify/index.ts
+++ src/modules/atom/portfolioModify/index.ts
@@ -1,5 +1,21 @@
 import { atom } from "recoil";
 
+export const container_list_modify_atom = atom<any>({
+  key: "container_text",
+  default: [
+    {
+      container_title: "",
+      container_text_list: [
+        {
+          box_title: "",
+          box_content: "",
+        },
+      ],
+      container_img_list: [],
+    },
+  ],
+});
+
 
 export const portfolioModifyList = atom({
     key: "portfolioModifyListAtom",
