4012a5fbefc2c06073803b9529c91d3eace2b994
JaeHyun <soehd0889@naver.com>
2022-02-05 23:33:48 +0900

    [MODIFY] 타입 수정

--- src/util/api/portfolio/portfolioModify.ts
+++ src/util/api/portfolio/portfolioModify.ts
@@ -2,7 +2,7 @@ import request from "../common/index";
 
 const token = `Bearer ${localStorage.getItem("access_token_portfolist")}`;
 
-export const portfolioModifySubmit = (data: any, portfolioId: string) => {
+export const portfolioModifySubmit = (data: any, portfolioId: number) => {
     return request({
       url: `/portfolio/${portfolioId}`,
       method: "put",
