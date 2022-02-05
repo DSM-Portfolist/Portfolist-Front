6092039622b6ba2d51e5856e8222f7a9d7353120
JaeHyun <soehd0889@naver.com>
2022-02-05 16:24:33 +0900

    [ADD] portfolio atom 생성

---
+++ src/modules/atom/portfolioModify/index.ts
@@ -0,0 +1,39 @@
+import { atom } from "recoil";
+
+
+export const portfolioModifyList = atom({
+    key: "portfolioModifyListAtom",
+    default: {
+      title: "",
+      introduce: "",
+      field: [1], //이거 없음
+      open: true, //이거 없음
+      more_info: [
+        {
+          name: "",
+          content: "",
+        },
+      ],
+      container_list: [
+        {
+          container_title: "",
+          container_text_list: [
+            {
+              box_title: "",
+              box_content: "",
+            },
+          ],
+          container_img_list: [],
+        },
+      ],
+      certificate_container_list: [
+        {
+          title: "",
+          certificate_list: [],
+        },
+      ],
+      link: "",
+      file: "",
+      thumbnail: "",
+    },
+  });
