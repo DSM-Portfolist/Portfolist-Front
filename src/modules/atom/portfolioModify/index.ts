8a09810b22d09c574e4dd0e8581088f023396619
강은빈 <kub0803@naver.com>
2022-02-10 10:08:12 +0900

    [REFACTOR] 타입 추가

--- src/modules/atom/portfolioModify/index.ts
+++ src/modules/atom/portfolioModify/index.ts
@@ -1,8 +1,7 @@
 import { atom } from "recoil";
 import { imageList } from "../../../components/portfolioMake/bannerContainer/items/ImageSelector";
-import {
-  BannderStateType,
-} from "../../../util/interface/portfolioPost/postType";
+import { PortfolioType } from "../../../util/interface/portfolio/portfolioDetailType";
+import { BannderStateType } from "../../../util/interface/portfolioPost/postType";
 
 export const container_list_modify_atom = atom<any>({
   key: "container_text_Modify",
@@ -28,39 +27,40 @@ export const bannerModifyImgAtom = atom<BannderStateType>({
   },
 });
 
-export const portfolioModifyList = atom({
-    key: "portfolioModifyListAtom",
-    default: {
-      title: "",
-      introduce: "",
-      field: [1], //이거 없음
-      open: true, //이거 없음
-      more_info: [
-        {
-          name: "",
-          content: "",
-        },
-      ],
-      container_list: [
-        {
-          container_title: "",
-          container_text_list: [
-            {
-              box_title: "",
-              box_content: "",
-            },
-          ],
-          container_img_list: [],
-        },
-      ],
-      certificate_container_list: [
-        {
-          title: "",
-          certificate_list: [""],
-        },
-      ],
-      link: "",
-      file: "",
-      thumbnail: "",
+export const portfolioModifyList = atom<PortfolioType>({
+  key: "portfolioModifyListAtom",
+  default: {
+    portfolio_id: 0,
+    certificate_container_list: [
+      {
+        title: "",
+        certificate_list: [""],
+      },
+    ],
+    container_list: [
+      {
+        container_title: "",
+        container_img_list: [""],
+        container_text_list: [{ box_title: "", box_content: "" }],
+      },
+    ],
+    create_date: "",
+    field: [""],
+    file: "",
+    icon: "",
+    introduce: "",
+    mine: false,
+    link: "",
+    more_info: [{ name: "", content: "" }],
+    user: {
+      name: "",
+      profile_img: "",
+      user_id: 0,
     },
+    title: "",
+    total_touching: 0,
+    touched: false,
+    open: false,
+    thumbnail: "",
+  },
 });
