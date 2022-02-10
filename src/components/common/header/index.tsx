d52808f70d6f878d4e3bda634be5963f149dabc0
강은빈 <kub0803@naver.com>
2022-02-11 01:45:25 +0900

    [ADD] 헤더 서브 메뉴 디자인 수정

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { Link, withRouter, useHistory } from "react-router-dom";
 import { Logo, Magnifier } from "../../../util/assets";
 import * as S from "./style";
-import SubMenu from "./SubMenu";
 import Notiication from "./notification/Notiication";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { searchBar } from "../../../modules/atom/header";
@@ -11,6 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { reload } from "../../../modules/atom/auth";
 import LoginCompontent from "./loginBeforeItem";
+import SubMenuBox from "./subMenu/SubMenu";
 
 const Header = () => {
   const [selectText, setSelectText] = useState<boolean>(true);
@@ -83,7 +83,7 @@ const Header = () => {
                 />
               </li>
               <Notiication />
-              <SubMenu />
+              <SubMenuBox />
             </ul>
           </S.Container>
         ) : (
