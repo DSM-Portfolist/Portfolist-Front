14148129067d1e2bdce9ab87c54abad4685e41d1
강은빈 <kub0803@naver.com>
2022-02-10 10:07:18 +0900

    [REFACTOR] fieldToId hook

---
+++ src/hook/fieldToIdHook.ts
@@ -0,0 +1,18 @@
+export const FieldToId = (fieldName: string) => {
+    switch (fieldName) {
+      case "FrontEnd":
+        return "1";
+      case "SERVER":
+        return "2";
+      case "ANDROID":
+        return "3";
+      case "AI":
+        return "4";
+      case "DATA ANALYSIS":
+        return "5";
+      case "DESIGN":
+        return "6";
+      case "DevOps":
+        return "7";
+    }
+  };
