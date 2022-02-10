fae1132837f6ef5b621aa1ddbd8dbcce46378829
강은빈 <kub0803@naver.com>
2022-02-10 23:13:41 +0900

    [REFACTOR] 터칭 api 코드 정리

--- src/util/api/portfolio/useTouching.ts
+++ src/util/api/portfolio/useTouching.ts
@@ -4,15 +4,11 @@ import { MAINURL } from "../common";
 const token = `Bearer ${localStorage.getItem("access_token_portfolist")}`;
 
 export const postTouching = async (id: number) => {
-  return await axios.post(
-    `${MAINURL}/touching/${id}`,
-    {},
-    {
-      headers: {
-        Authorization: token,
-      },
-    }
-  );
+  return await axios.post(`${MAINURL}/touching/${id}`, {
+    headers: {
+      Authorization: token,
+    },
+  });
 };
 
 export const deleteTouching = async (id: number) => {
